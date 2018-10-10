9cd01de17e66037c1a7864b4124113ccb5269414
Niedermann IT-Dienstleistungen <info@niedermann.it>
2018-10-10 09:34:17 +0200

    Use SingleSignOn as git submodule

--- app/src/main/java/it/niedermann/nextcloud/deck/api/DeckAPI.java
+++ app/src/main/java/it/niedermann/nextcloud/deck/api/DeckAPI.java
@@ -5,9 +5,7 @@ import java.util.List;
 
 import io.reactivex.Observable;
 import it.niedermann.nextcloud.deck.model.Board;
-import retrofit2.http.DELETE;
 import retrofit2.http.GET;
-import retrofit2.http.POST;
 
 public interface DeckAPI {
 
