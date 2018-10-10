57479aaad41499734896111266f098e0bceeda8e
Niedermann IT-Dienstleistungen <info@niedermann.it>
2018-10-10 09:18:31 +0200

    Fix account detection

--- app/src/main/java/it/niedermann/nextcloud/deck/MainActivity.java
+++ app/src/main/java/it/niedermann/nextcloud/deck/MainActivity.java
@@ -15,6 +15,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.nextcloud.android.sso.api.NextcloudAPI;
 import com.nextcloud.android.sso.exceptions.NextcloudFilesAppAccountNotFoundException;
 import com.nextcloud.android.sso.exceptions.NoCurrentAccountSelectedException;
@@ -62,91 +63,44 @@ public class MainActivity extends AppCompatActivity
 
         this.dataBaseAdapter = DataBaseAdapter.getInstance(this.getApplicationContext());
         if(this.dataBaseAdapter.hasAccounts()) {
-            DeckAPI mApi;
+
+            String accountName = dataBaseAdapter.readAccounts().get(0).getName();
+            SingleAccountHelper.setCurrentAccount(getApplicationContext(), accountName);
+
             SingleSignOnAccount account = null;
             try {
                 account = SingleAccountHelper.getCurrentSingleSignOnAccount(getApplicationContext());
                 //SingleSignOnAccount ssoAccount =
                 //        AccountImporter.getAuthTokenInSeparateThread(context, account);
-                NextcloudAPI nextcloudAPI = new NextcloudAPI(getApplicationContext(), account, new Gson(), new NextcloudAPI.ApiConnectedListener() {
+                mNextcloudAPI = new NextcloudAPI(getApplicationContext(), account, new Gson(), new NextcloudAPI.ApiConnectedListener() {
 
                     @Override
                     public void onConnected() {
-
+                        try{
+                            DeckAPI mApi = new DeckAPI_SSO(mNextcloudAPI);
+                            mApi.boards().subscribe(new Consumer<List<Board>>() {
+                                @Override
+                                public void accept(List<Board> boards) throws Exception {
+                                    Log.v("Deck", "=============================================================");
+                                    Log.v("Deck", "" + boards.size());
+                                }
+                            });
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
                     }
 
                     @Override
                     public void onError(Exception e) {
+                    }
+                });
 
-                    }
-                });
-                mApi = new DeckAPI_SSO(nextcloudAPI);
-                mApi.boards().subscribe(new Consumer<List<Board>>() {
-                    @Override
-                    public void accept(List<Board> boards) throws Exception {
-                        Log.v("Deck", "=============================================================");
-                        Log.v("Deck", "" + boards.size());
-                    }
-                });
             } catch (NextcloudFilesAppAccountNotFoundException e) {
                 e.printStackTrace();
             } catch (NoCurrentAccountSelectedException e) {
                 e.printStackTrace();
             }
 
-
-
-            /*
-            new AsyncTask<Void, Void, Void>() {
-
-                @Override
-                protected Void doInBackground(Void... voids) {
-                    String accountName = dataBaseAdapter.readAccounts().get(0).getName();
-                    SingleAccountHelper.setCurrentAccount(getApplicationContext(), accountName);
-                    try {
-                        Log.v("Deck", "===============================================================================");
-                        Log.v("Deck", "Bestehenden Account erkannt: " + SingleAccountHelper.getCurrentSingleSignOnAccount(getApplicationContext()).name);
-
-                        SingleSignOnAccount acc = SingleAccountHelper.getCurrentSingleSignOnAccount(getApplicationContext());
-
-                        mNextcloudAPI= new NextcloudAPI(getApplicationContext(), acc, new Gson(),  new NextcloudAPI.ApiConnectedListener() {
-                            @Override
-                            public void onConnected() {
-
-                                NextcloudRequest nextcloudRequest = new NextcloudRequest.Builder()
-                                        .setMethod("GET")
-                                        .setUrl("/index.php/apps/deck/boards")
-                                        .build();
-
-                                try {
-                                    InputStream inputStream = mNextcloudAPI.performNetworkRequest(nextcloudRequest);
-                                    while(inputStream.available() > 0) {
-                                        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
-
-                                        Log.v("Deck", br.readLine());
-                                        // TODO do something useful with the data here..
-                                        // like writing it to a file..?
-                                    }
-                                    inputStream.close();
-                                } catch (Exception e) {
-                                    e.printStackTrace();
-                                }
-                            }
-
-                            @Override
-                            public void onError(Exception ex) {
-                                // TODO handle error here..
-                                ex.printStackTrace();
-                            }
-                        });
-                    } catch (NextcloudFilesAppAccountNotFoundException e) {
-                        e.printStackTrace();
-                    } catch (NoCurrentAccountSelectedException e) {
-                        e.printStackTrace();
-                    }
-                    return null;
-                }
-            }.doInBackground();*/
         } else {
             loginDialogFragment = new LoginDialogFragment();
             loginDialogFragment.show(this.getSupportFragmentManager(), "NoticeDialogFragment");
