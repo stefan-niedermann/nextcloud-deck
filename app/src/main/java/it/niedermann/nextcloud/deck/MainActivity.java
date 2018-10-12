ef25fdcb9efe5eae38eef4e9822163d42d34da50
Niedermann IT-Dienstleistungen <info@niedermann.it>
2018-10-12 11:18:48 +0200

    Remove dead code and use ButterKnife

--- app/src/main/java/it/niedermann/nextcloud/deck/MainActivity.java
+++ app/src/main/java/it/niedermann/nextcloud/deck/MainActivity.java
@@ -20,29 +20,33 @@ import com.nextcloud.android.sso.model.SingleSignOnAccount;
 
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import io.reactivex.functions.Consumer;
 import it.niedermann.nextcloud.deck.api.ApiProvider;
-import it.niedermann.nextcloud.deck.api.DeckAPI;
-import it.niedermann.nextcloud.deck.api.DeckAPI_SSO;
 import it.niedermann.nextcloud.deck.model.DataBaseAdapter;
 import it.niedermann.nextcloud.deck.model.board.Board;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener, NextcloudAPI.ApiConnectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+
+    @BindView(R.id.toolbar) Toolbar toolbar;
+    @BindView(R.id.fab) FloatingActionButton fab;
+    @BindView(R.id.drawer_layout) DrawerLayout drawer;
+    @BindView(R.id.nav_view) NavigationView navigationView;
+
     private NextcloudAPI mNextcloudAPI;
     private DataBaseAdapter dataBaseAdapter;
     private LoginDialogFragment loginDialogFragment;
-    private SingleSignOnAccount account = null;
     ApiProvider provider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        ButterKnife.bind(this);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -51,18 +55,16 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
         this.dataBaseAdapter = DataBaseAdapter.getInstance(this.getApplicationContext());
-        if(this.dataBaseAdapter.hasAccounts()) {
 
+        if(this.dataBaseAdapter.hasAccounts()) {
             String accountName = dataBaseAdapter.readAccounts().get(0).getName();
             SingleAccountHelper.setCurrentAccount(getApplicationContext(), accountName);
             provider = new ApiProvider(getApplicationContext(), new NextcloudAPI.ApiConnectedListener() {
@@ -71,7 +73,7 @@ public class MainActivity extends AppCompatActivity
                     final Consumer<List<Board>> consumer = new Consumer<List<Board>>() {
                         @Override
                         public void accept(List<Board> boards) throws Exception {
-                            Log.e("Deck", "=============================================================");
+                            Log.e("Deck", "================= Boards ============================================");
                             Log.e("Deck", "" + boards.get(0).getTitle());
                         }
                     };
@@ -92,7 +94,7 @@ public class MainActivity extends AppCompatActivity
 
                 @Override
                 public void onError(Exception ex) {
-
+                    ex.printStackTrace();
                 }
             });
 
@@ -100,6 +102,12 @@ public class MainActivity extends AppCompatActivity
             loginDialogFragment = new LoginDialogFragment();
             loginDialogFragment.show(this.getSupportFragmentManager(), "NoticeDialogFragment");
         }
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 
     public void onAccountChoose(SingleSignOnAccount account) {
@@ -109,7 +117,6 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -159,29 +166,7 @@ public class MainActivity extends AppCompatActivity
 
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
-    }
-
-    @Override
-    public void onConnected() {
-        try{
-            DeckAPI mApi = new DeckAPI_SSO(mNextcloudAPI);
-            mApi.boards().subscribe(new Consumer<List<Board>>() {
-                @Override
-                public void accept(List<Board> boards) throws Exception {
-                    Log.e("Deck", "=============================================================");
-                    Log.e("Deck", "" + boards.size());
-                }
-            });
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public void onError(Exception ex) {
-        ex.printStackTrace();
     }
 }
--- app/src/main/java/it/niedermann/nextcloud/deck/ui/board/fragment/TaskViewModel.java
+++
@@ -1,7 +0,0 @@
-package it.niedermann.nextcloud.deck.ui.board.fragment;
-
-import android.arch.lifecycle.ViewModel;
-
-public class TaskViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
