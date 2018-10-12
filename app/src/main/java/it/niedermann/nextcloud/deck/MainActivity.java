c0aa69f4823fcc1c6b55347ba08639b468fdd841
Niedermann IT-Dienstleistungen <info@niedermann.it>
2018-10-12 13:47:43 +0200

    Move boards list to drawer

--- app/src/main/java/it/niedermann/nextcloud/deck/MainActivity.java
+++ app/src/main/java/it/niedermann/nextcloud/deck/MainActivity.java
@@ -169,19 +169,6 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
 
         drawer.closeDrawer(GravityCompat.START);
         return true;
