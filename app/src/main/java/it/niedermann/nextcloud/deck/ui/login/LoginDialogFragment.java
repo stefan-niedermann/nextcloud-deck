155866bc37c6847069bb42ddd45fbb4bea673900
Niedermann IT-Dienstleistungen <info@niedermann.it>
2018-11-10 22:48:00 +0100

    redirect to Play Store when files app is not installed

--- app/src/main/java/it/niedermann/nextcloud/deck/ui/login/LoginDialogFragment.java
+++ app/src/main/java/it/niedermann/nextcloud/deck/ui/login/LoginDialogFragment.java
@@ -2,6 +2,7 @@ package it.niedermann.nextcloud.deck.ui.login;
 
 import android.app.Dialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -12,6 +13,7 @@ import com.nextcloud.android.sso.AccountImporter;
 import com.nextcloud.android.sso.exceptions.NextcloudFilesAppNotInstalledException;
 import com.nextcloud.android.sso.model.SingleSignOnAccount;
 
+import it.niedermann.nextcloud.deck.R;
 import it.niedermann.nextcloud.deck.ui.MainActivity;
 
 public class LoginDialogFragment extends DialogFragment {
@@ -23,7 +25,7 @@ public class LoginDialogFragment extends DialogFragment {
         } catch (NextcloudFilesAppNotInstalledException e) {
             Log.w("Deck", "=============================================================");
             Log.w("Deck", "Nextcloud app is not installed. Cannot choose account");
-            // ToDo redirect to Play Store
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.url_files))));
             e.printStackTrace();
         }
         return super.onCreateDialog(savedInstanceState);
