8b0a2b1127c7294b5c1b16215d9894dabe5c810d
Niedermann IT-Dienstleistungen <info@niedermann.it>
2018-11-10 21:46:43 +0100

    Temporary remove options menu

--- app/src/main/java/it/niedermann/nextcloud/deck/ui/MainActivity.java
+++ app/src/main/java/it/niedermann/nextcloud/deck/ui/MainActivity.java
@@ -143,12 +143,12 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    @Override
+    /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
-    }
+    }*/
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
