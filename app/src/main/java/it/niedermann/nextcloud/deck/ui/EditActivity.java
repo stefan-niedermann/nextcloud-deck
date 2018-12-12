5304a7571c435ce3d36d50073c4b08429491fc31
Niedermann IT-Dienstleistungen <info@niedermann.it>
2018-12-12 17:16:39 +0100

    Fix setContentView, multiline description, icon for description

--- app/src/main/java/it/niedermann/nextcloud/deck/ui/EditActivity.java
+++ app/src/main/java/it/niedermann/nextcloud/deck/ui/EditActivity.java
@@ -1,15 +1,14 @@
 package it.niedermann.nextcloud.deck.ui;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 
 import it.niedermann.nextcloud.deck.R;
 
 public class EditActivity extends AppCompatActivity {
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
     }
 }
