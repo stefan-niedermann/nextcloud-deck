3b1e8a3081cb1e49bc8290fb204ec71d83a8f993
stefan-niedermann <info@niedermann.it>
2019-03-12 16:18:12 +0100

    Remove dead dependencies, add dummy methods for create buttons

--- app/src/main/java/it/niedermann/nextcloud/deck/ui/BottomSheetCreateFragment.java
+++ app/src/main/java/it/niedermann/nextcloud/deck/ui/BottomSheetCreateFragment.java
@@ -6,8 +6,10 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.google.android.material.snackbar.Snackbar;
 
 import androidx.annotation.NonNull;
+import butterknife.OnClick;
 import it.niedermann.nextcloud.deck.R;
 
 
@@ -24,5 +26,20 @@ public class BottomSheetCreateFragment extends BottomSheetDialogFragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.bottom_sheet_create, container, false);
+    }
+
+    @OnClick(R.id.create_card)
+    void onCreateCardClicked() {
+        Snackbar.make(this.getView(), "Creating cards is not yet supported.", Snackbar.LENGTH_SHORT);
+    }
+
+    @OnClick(R.id.create_stack)
+    void onCreateStackClicked() {
+        Snackbar.make(this.getView(), "Creating stacks is not yet supported.", Snackbar.LENGTH_SHORT);
+    }
+
+    @OnClick(R.id.create_board)
+    void onCreateBoardClicked() {
+        Snackbar.make(this.getView(), "Creating boards is not yet supported.", Snackbar.LENGTH_SHORT);
     }
 }
