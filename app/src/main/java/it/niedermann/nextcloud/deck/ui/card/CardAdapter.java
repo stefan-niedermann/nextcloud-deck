fa2c438dea93bfb564386af87ee303ff1ce2f4e8
stefan-niedermann <info@niedermann.it>
2019-03-14 12:48:04 +0100

    Use only first letter to display tags in boards view
    First letter is needed for accessability (e. g. problems with differenciating colors)

--- app/src/main/java/it/niedermann/nextcloud/deck/ui/card/CardAdapter.java
+++ app/src/main/java/it/niedermann/nextcloud/deck/ui/card/CardAdapter.java
@@ -125,7 +125,7 @@ public class CardAdapter extends RecyclerView.Adapter<CardAdapter.CardViewHolder
         if (card.getLabels()!= null && card.getLabels().size() > 0) {
             for (Label label : card.getLabels()) {
                 chip = new Chip(context);
-                chip.setText(label.getTitle());
+                chip.setText(label.getTitle().substring(0, 1));
 
                 try {
                     int labelColor = Color.parseColor("#" + label.getColor());
