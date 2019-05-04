421ed34ad707fb39e602181ed7402cb352f587e1
stefan-niedermann <info@niedermann.it>
2019-05-04 21:28:26 +0200

    #43 Create boards
    When selecting a color, the previous color has to be deselected

--- app/src/main/java/it/niedermann/nextcloud/deck/ui/board/BoardCreateDialogFragment.java
+++ app/src/main/java/it/niedermann/nextcloud/deck/ui/board/BoardCreateDialogFragment.java
@@ -1,6 +1,7 @@
 package it.niedermann.nextcloud.deck.ui.board;
 
 import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -10,6 +11,9 @@ import android.widget.LinearLayout;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
+
+import java.util.Objects;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import it.niedermann.nextcloud.deck.R;
@@ -18,10 +22,13 @@ import it.niedermann.nextcloud.deck.util.ViewUtil;
 
 public class BoardCreateDialogFragment extends DialogFragment {
 
+    private Context context;
     private String selectedColor;
+    private String previouslySelectedColor;
+    private ImageView previouslySelectedImageView;
 
     @BindView(R.id.input)
-    EditText input;
+    EditText boardTitle;
     @BindView(R.id.colorPicker)
     LinearLayout colorPicker;
 
@@ -29,7 +36,7 @@ public class BoardCreateDialogFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         View view = getActivity().getLayoutInflater().inflate(R.layout.dialog_board_create, null);
-
+        this.context = Objects.requireNonNull(getContext());
         ButterKnife.bind(this, view);
 
         initColorChooser();
@@ -41,7 +48,7 @@ public class BoardCreateDialogFragment extends DialogFragment {
                     // Do something else
                 })
                 .setPositiveButton(R.string.simple_create, (dialog, which) -> {
-                    ((MainActivity) getActivity()).onCreateBoard(input.getText().toString(), selectedColor);
+                    ((MainActivity) getActivity()).onCreateBoard(boardTitle.getText().toString(), selectedColor);
                 })
                 .create();
     }
@@ -52,14 +59,20 @@ public class BoardCreateDialogFragment extends DialogFragment {
 
         String[] colors = getResources().getStringArray(R.array.board_default_colors);
 
+        // TODO refactor color chooser as own View Component
         for (int i = 0; i < colors.length; i++) {
             final String color = colors[i];
             ImageView image = new ImageView(getContext());
             image.setOnClickListener((imageView) -> {
-                image.setImageDrawable(ViewUtil.getTintedImageView(getContext(), R.drawable.circle_alpha_check_36dp, color));
+                if (previouslySelectedImageView != null) { // null when first selection
+                    previouslySelectedImageView.setImageDrawable(ViewUtil.getTintedImageView(this.context, R.drawable.circle_grey600_36dp, previouslySelectedColor));
+                }
+                image.setImageDrawable(ViewUtil.getTintedImageView(this.context, R.drawable.circle_alpha_check_36dp, color));
                 selectedColor = color;
+                this.previouslySelectedColor = color;
+                this.previouslySelectedImageView = image;
             });
-            image.setImageDrawable(ViewUtil.getTintedImageView(getContext(), R.drawable.circle_grey600_36dp, color));
+            image.setImageDrawable(ViewUtil.getTintedImageView(this.context, R.drawable.circle_grey600_36dp, color));
             if (i < colors.length - 1) {
                 image.setLayoutParams(lp);
             }
