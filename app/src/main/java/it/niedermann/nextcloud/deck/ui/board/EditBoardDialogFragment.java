7df89b45322fd23c5485d217a1635f7e35b851e7
stefan-niedermann <info@niedermann.it>
2019-05-09 14:03:35 +0200

    Move ColorChooser to own ViewComponent

--- app/src/main/java/it/niedermann/nextcloud/deck/ui/board/EditBoardDialogFragment.java
+++ app/src/main/java/it/niedermann/nextcloud/deck/ui/board/EditBoardDialogFragment.java
@@ -12,8 +12,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
 
-import com.google.android.flexbox.FlexboxLayout;
-
 import java.util.Objects;
 
 import butterknife.BindView;
@@ -24,7 +22,7 @@ import it.niedermann.nextcloud.deck.model.Board;
 import it.niedermann.nextcloud.deck.model.full.FullBoard;
 import it.niedermann.nextcloud.deck.persistence.sync.SyncManager;
 import it.niedermann.nextcloud.deck.ui.MainActivity;
-import it.niedermann.nextcloud.deck.util.ViewUtil;
+import it.niedermann.nextcloud.deck.ui.helper.colorchooser.ColorChooser;
 
 public class EditBoardDialogFragment extends DialogFragment {
 
@@ -32,7 +30,8 @@ public class EditBoardDialogFragment extends DialogFragment {
     private static final String KEY_BOARD_ID = "board_id";
     private static final Long NO_BOARD_ID = -1L;
 
-    @NonNull private Activity activity;
+    @NonNull
+    private Activity activity;
     private Context context;
     private SyncManager syncManager;
 
@@ -44,8 +43,9 @@ public class EditBoardDialogFragment extends DialogFragment {
 
     @BindView(R.id.input)
     EditText boardTitle;
-    @BindView(R.id.colorPicker)
-    FlexboxLayout colorPicker;
+
+    @BindView(R.id.colorChooser)
+    ColorChooser colorChooser;
 
     @NonNull
     @Override
@@ -59,9 +59,9 @@ public class EditBoardDialogFragment extends DialogFragment {
         accountId = Objects.requireNonNull(getArguments()).getLong(KEY_ACCOUNT_ID);
         syncManager = new SyncManager(this.context, this.activity);
 
-        AlertDialog.Builder dialogBuilder =  new AlertDialog.Builder(this.activity);
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this.activity);
 
-        if(NO_BOARD_ID.equals(boardId)) {
+        if (NO_BOARD_ID.equals(boardId)) {
             initColorChooser();
             dialogBuilder.setTitle(R.string.create_board);
             dialogBuilder.setPositiveButton(R.string.simple_create, (dialog, which) -> ((MainActivity) getActivity()).onCreateBoard(boardTitle.getText().toString(), selectedColor));
@@ -75,7 +75,7 @@ public class EditBoardDialogFragment extends DialogFragment {
             DeckLog.log("--- request with | " + boardId);
             syncManager.getFullBoardById(accountId, boardId).observe(EditBoardDialogFragment.this, (FullBoard fb) -> {
                 DeckLog.log("--- localid | id | " + fb.getLocalId() + " | " + fb.getId());
-                if(fb.board != null) {
+                if (fb.board != null) {
                     this.fullBoard = fb;
                     this.boardTitle.setText(this.fullBoard.board.getTitle());
                     initColorChooser();
@@ -85,7 +85,8 @@ public class EditBoardDialogFragment extends DialogFragment {
 
         return dialogBuilder
                 .setView(view)
-                .setNegativeButton(R.string.simple_cancel, (dialog, which) -> {})
+                .setNegativeButton(R.string.simple_cancel, (dialog, which) -> {
+                })
                 .create();
     }
 
@@ -112,34 +113,9 @@ public class EditBoardDialogFragment extends DialogFragment {
     }
 
     private void initColorChooser() {
-        String[] colors = getResources().getStringArray(R.array.board_default_colors);
-
-        // TODO refactor color chooser as own View Component
-        for (final String color : colors) {
-            ImageView image = new ImageView(getContext());
-            image.setOnClickListener((imageView) -> {
-                if (previouslySelectedImageView != null) { // null when first selection
-                    previouslySelectedImageView.setImageDrawable(ViewUtil.getTintedImageView(this.context, R.drawable.circle_grey600_36dp, previouslySelectedColor));
-                }
-                image.setImageDrawable(ViewUtil.getTintedImageView(this.context, R.drawable.circle_alpha_check_36dp, color));
-                selectedColor = color;
-                this.previouslySelectedColor = color;
-                this.previouslySelectedImageView = image;
-            });
-            if(this.fullBoard != null && color.toLowerCase().equals("#" + this.fullBoard.board.getColor().toLowerCase())) {
-                this.selectedColor = color;
-                this.previouslySelectedColor = color;
-                this.previouslySelectedImageView = image;
-                image.setImageDrawable(ViewUtil.getTintedImageView(this.context, R.drawable.circle_alpha_check_36dp, color));
-            } else {
-                image.setImageDrawable(ViewUtil.getTintedImageView(this.context, R.drawable.circle_grey600_36dp, color));
-            }
-            colorPicker.addView(image);
-        }
-
         // Preselect when create a new board
-        if(fullBoard == null && colorPicker.getChildCount() > 0) {
-            colorPicker.getChildAt(0).performClick();
+        if (fullBoard == null && colorChooser.getChildCount() > 0) {
+            colorChooser.selectColor(0);
         }
     }
 }
---
+++ app/src/main/java/it/niedermann/nextcloud/deck/ui/helper/colorchooser/ColorChooser.java
@@ -0,0 +1,84 @@
+package it.niedermann.nextcloud.deck.ui.helper.colorchooser;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
+
+import com.google.android.flexbox.FlexboxLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import it.niedermann.nextcloud.deck.R;
+import it.niedermann.nextcloud.deck.util.ViewUtil;
+
+public class ColorChooser extends LinearLayout {
+
+    private Context context;
+
+    @BindView(R.id.colorPicker)
+    FlexboxLayout colorPicker;
+
+    private String selectedColor;
+    private String previouslySelectedColor;
+    private ImageView previouslySelectedImageView;
+
+
+    public ColorChooser(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        this.context = context;
+
+        TypedArray a = context.obtainStyledAttributes(attrs,
+                R.styleable.ColorChooser, 0, 0);
+        String[] colors = getResources().getStringArray(a.getResourceId(R.styleable.ColorChooser_colors, 0));
+        String defaultColor = a.getString(R.styleable.ColorChooser_defaultColor);
+        a.recycle();
+
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.color_chooser, this, true);
+        ButterKnife.bind(this);
+        initDefaultColors(colors, defaultColor);
+    }
+
+    private void initDefaultColors(String[] colors, String defaultColor) {
+        for (final String color : colors) {
+            ImageView image = new ImageView(getContext());
+            image.setOnClickListener((imageView) -> {
+                if (previouslySelectedImageView != null) { // null when first selection
+                    previouslySelectedImageView.setImageDrawable(ViewUtil.getTintedImageView(this.context, R.drawable.circle_grey600_36dp, previouslySelectedColor));
+                }
+                image.setImageDrawable(ViewUtil.getTintedImageView(this.context, R.drawable.circle_alpha_check_36dp, color));
+                selectedColor = color;
+                this.previouslySelectedColor = color;
+                this.previouslySelectedImageView = image;
+            });
+            if(color.toLowerCase().equals("#" + defaultColor.toLowerCase())) {
+                this.selectedColor = color;
+                this.previouslySelectedColor = color;
+                this.previouslySelectedImageView = image;
+                image.setImageDrawable(ViewUtil.getTintedImageView(this.context, R.drawable.circle_alpha_check_36dp, color));
+            } else {
+                image.setImageDrawable(ViewUtil.getTintedImageView(this.context, R.drawable.circle_grey600_36dp, color));
+            }
+            colorPicker.addView(image);
+        }
+
+        // Preselect when create a new board
+//        if(colorPicker.getChildCount() > 0) {
+//            colorPicker.getChildAt(0).performClick();
+//        }
+    }
+
+    public void selectColor(int index) {
+        if(colorPicker.getChildCount() > 0) {
+            colorPicker.getChildAt(0);
+        } else {
+            throw new IndexOutOfBoundsException("index not available");
+        }
+    }
+}
