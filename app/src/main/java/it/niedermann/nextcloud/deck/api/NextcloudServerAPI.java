ec9491dd8d5284ad527b97700de9d34860bca318
desperateCoder <echotodevnull@gmail.com>
2019-07-21 14:01:41 +0200

    getting capabilities works.

--- app/src/main/java/it/niedermann/nextcloud/deck/api/NextcloudServerAPI.java
+++ app/src/main/java/it/niedermann/nextcloud/deck/api/NextcloudServerAPI.java
@@ -4,14 +4,9 @@ package it.niedermann.nextcloud.deck.api;
 import io.reactivex.Observable;
 import it.niedermann.nextcloud.deck.model.ocs.Capabilities;
 import retrofit2.http.GET;
-import retrofit2.http.Headers;
 
 public interface NextcloudServerAPI {
 
-    @Headers({
-            "OCS-APIRequest: true"
-    })
     @GET("cloud/capabilities?format=json")
     Observable<Capabilities> getCapabilities();
-
 }
