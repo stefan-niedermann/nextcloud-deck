ad13f0262ce3303b4491481d762cee22ddf19d53
stefan-niedermann <info@niedermann.it>
2019-09-14 22:54:14 +0200

    Adjust empty text for no attachments

--- app/src/main/java/it/niedermann/nextcloud/deck/ui/card/CardActivityFragment.java
+++ app/src/main/java/it/niedermann/nextcloud/deck/ui/card/CardActivityFragment.java
@@ -1,11 +1,12 @@
 package it.niedermann.nextcloud.deck.ui.card;
 
 import android.os.Bundle;
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
