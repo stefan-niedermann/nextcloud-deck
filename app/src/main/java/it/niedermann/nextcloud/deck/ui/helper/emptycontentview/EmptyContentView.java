8970aed5c18631e5bf7cacb1c5ff4e0d842616b8
stefan-niedermann <info@niedermann.it>
2019-12-12 17:52:56 +0100

    #224 Make reusable component for empty content views
    Default value handling

--- app/src/main/java/it/niedermann/nextcloud/deck/ui/helper/emptycontentview/EmptyContentView.java
+++ app/src/main/java/it/niedermann/nextcloud/deck/ui/helper/emptycontentview/EmptyContentView.java
@@ -4,17 +4,21 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.StringRes;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import it.niedermann.nextcloud.deck.R;
 
 public class EmptyContentView extends RelativeLayout {
+
+    private static final int NO_DESCRIPTION = -1;
 
     @BindView(R.id.title)
     TextView title;
@@ -34,9 +38,16 @@ public class EmptyContentView extends RelativeLayout {
 
         TypedArray a = context.obtainStyledAttributes(attrs,
                 R.styleable.EmptyContentView, 0, 0);
-        title.setText(getResources().getString(a.getResourceId(R.styleable.EmptyContentView_title, R.string.app_name_short)));
-        description.setText(getResources().getString(a.getResourceId(R.styleable.EmptyContentView_description, R.string.app_name)));
-        image.setImageDrawable(getResources().getDrawable(a.getResourceId(R.styleable.EmptyContentView_image, R.drawable.ic_launcher_foreground)));
+
+        @StringRes int descriptionRes = a.getResourceId(R.styleable.EmptyContentView_description, NO_DESCRIPTION);
+
+        title.setText(getResources().getString(a.getResourceId(R.styleable.EmptyContentView_title, R.string.no_content)));
+        if (descriptionRes == NO_DESCRIPTION) {
+            description.setVisibility(View.GONE);
+        } else {
+            description.setText(getResources().getString(descriptionRes));
+        }
+        image.setImageDrawable(getResources().getDrawable(a.getResourceId(R.styleable.EmptyContentView_image, R.drawable.ic_app_logo)));
         a.recycle();
     }
 }
