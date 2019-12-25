ff898562bf46727f231212b9c77fa3337b95743c
Randy Olive <randy.jr.olive@gmail.com>
2019-12-25 14:48:18 -0500

    fixed custom color picker to correctly show a custom color when that board has a custom color selected
    Signed-off-by: Randy Olive <randy.jr.olive@gmail.com>

--- app/src/main/java/it/niedermann/nextcloud/deck/ui/helper/colorchooser/ColorChooser.java
+++ app/src/main/java/it/niedermann/nextcloud/deck/ui/helper/colorchooser/ColorChooser.java
@@ -30,6 +30,8 @@ public class ColorChooser extends LinearLayout {
     private String previouslySelectedColor;
     private ImageView previouslySelectedImageView;
 
+    private Boolean isCustomColor = false;
+
 
     public ColorChooser(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -45,42 +47,64 @@ public class ColorChooser extends LinearLayout {
         inflater.inflate(R.layout.widget_color_chooser, this, true);
         ButterKnife.bind(this);
         initDefaultColors();
+
     }
 
     private void initDefaultColors() {
-        // initializes the color picker with the default values from board_default_colors (held in colors array)
-        // by looping through the array and creating an ImageView for each color
+        // initializes the color picker with the default values from board_default_colors (in the colors array)
+        // by looping through the array and creating an ImageView icon for each color
         for (final String color : colors) {
             ImageView image = new ImageView(getContext());
             image.setOnClickListener((View imageView) -> {
-                if (previouslySelectedImageView != null) { // null when first selection
+                if (previouslySelectedImageView != null) {
                     previouslySelectedImageView.setImageDrawable(ViewUtil.getTintedImageView(this.context, R.drawable.circle_grey600_36dp, previouslySelectedColor));
                 }
+                // when clicked -> sets this as the chosen color and adds the check icon to it
                 image.setImageDrawable(ViewUtil.getTintedImageView(this.context, R.drawable.circle_alpha_check_36dp, color));
                 selectedColor = color;
                 this.previouslySelectedColor = color;
                 this.previouslySelectedImageView = image;
+                //Log.d("watch", "color: " + color);
             });
+            // finally, sets the image to circle_grey so that it can be tinted to "color". This image
+            // is then added to the colorPicker
             image.setImageDrawable(ViewUtil.getTintedImageView(this.context, R.drawable.circle_grey600_36dp, color));
             colorPicker.addView(image);
         }
-        // initializes a final image for the custom color chooser
+    }
+
+    private void initCustomColorChooser() {
+        // initializes a final image icon for the custom color chooser and, if already set,
+        // will set the icon color to the custom color
         ImageView customColorChooser = new ImageView(getContext());
-        customColorChooser.setImageDrawable(ViewUtil.getTintedImageView(this.context, R.drawable.circle_alpha_colorize_36dp, R.color.board_default_custom_color));
+        Log.d("watch", "custom color set: " + isCustomColor);
+        if (isCustomColor == false) {
+            customColorChooser.setImageDrawable(ViewUtil.getTintedImageView(this.context, R.drawable.circle_alpha_colorize_36dp, R.color.board_default_custom_color));
+        } else {
+            customColorChooser.setImageDrawable(ViewUtil.getTintedImageView(this.context, R.drawable.circle_alpha_colorize_36dp, this.selectedColor));
+        }
         colorPicker.addView(customColorChooser);
         customColorChooser.setOnClickListener((View imageView) -> {
-            Log.d("watch", "button click color chooser icon");
+            Log.d("watch", "launch custom color chooser icon");
         });
     }
 
     public void selectColor(String newColor) {
         selectedColor = newColor;
+        Log.d("watch", "color: " + this.selectedColor);
+        // checks if the new color is one of the default colors in the colors array. If a match is
+        // found then that color is selected from the color icons
         for (int i = 0; i < colors.length; i++) {
             if (colors[i].equals(newColor)) {
+                initCustomColorChooser(); // adds custom color picker, with default color
                 colorPicker.getChildAt(i).performClick();
                 return;
             }
         }
+        // if the board color is not found to be a default color, then a custom color is assumed and
+        // the custom color chooser is given setup for this
+        isCustomColor = true;
+        initCustomColorChooser(); // adds custom color picker, with custom color
     }
 
     public String getSelectedColor() {
