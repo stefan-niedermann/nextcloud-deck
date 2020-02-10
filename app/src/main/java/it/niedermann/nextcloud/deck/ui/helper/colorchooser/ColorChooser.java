bf0409c080060c7ccb27da6d736e32f9bf591828
desperateCoder <echotodevnull@gmail.com>
2020-02-10 15:30:39 +0100

    merge PR #247

--- app/src/main/java/it/niedermann/nextcloud/deck/ui/helper/colorchooser/ColorChooser.java
+++ app/src/main/java/it/niedermann/nextcloud/deck/ui/helper/colorchooser/ColorChooser.java
@@ -4,12 +4,17 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;
 
 import com.google.android.flexbox.FlexboxLayout;
+import com.skydoves.colorpickerview.ColorEnvelope;
+import com.skydoves.colorpickerview.ColorPickerView;
+import com.skydoves.colorpickerview.listeners.ColorEnvelopeListener;
+import com.skydoves.colorpickerview.sliders.BrightnessSlideBar;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -28,6 +33,10 @@ public class ColorChooser extends LinearLayout {
     private String previouslySelectedColor;
     private ImageView previouslySelectedImageView;
 
+    private Boolean hasCustomColor = false;
+    private ColorPickerView customColorPicker;
+    private BrightnessSlideBar brightnessSlideBar;
+    private ImageView customColorChooser;
 
     public ColorChooser(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -43,6 +52,22 @@ public class ColorChooser extends LinearLayout {
         inflater.inflate(R.layout.widget_color_chooser, this, true);
         ButterKnife.bind(this);
         initDefaultColors();
+
+        customColorPicker = (ColorPickerView) findViewById(R.id.customColorPicker);
+        brightnessSlideBar = findViewById(R.id.brightnessSlide);
+
+        customColorPicker.attachBrightnessSlider(brightnessSlideBar);
+
+        customColorPicker.setColorListener(new ColorEnvelopeListener() {
+            @Override
+            public void onColorSelected(ColorEnvelope envelope, boolean fromUser) {
+                String customColor =  "#" + envelope.getHexCode().substring(2);
+                selectedColor = customColor;
+                previouslySelectedColor = customColor;
+                //previouslySelectedColor = customColor;
+                customColorChooser.setImageDrawable(ViewUtil.getTintedImageView(context, R.drawable.circle_alpha_colorize_36dp, selectedColor));
+                }
+        });
     }
 
     private void initDefaultColors() {
@@ -62,14 +87,36 @@ public class ColorChooser extends LinearLayout {
         }
     }
 
+    private void initCustomColorChooser() {
+        // initializes a final image icon for the custom color chooser and, if already set,
+        // will set the icon color to the custom color
+        customColorChooser = new ImageView(getContext());
+        if (!hasCustomColor) {
+            customColorChooser.setImageDrawable(ViewUtil.getTintedImageView(this.context, R.drawable.circle_alpha_colorize_36dp, R.color.board_default_custom_color));
+        } else {
+            customColorChooser.setImageDrawable(ViewUtil.getTintedImageView(this.context, R.drawable.circle_alpha_colorize_36dp, this.selectedColor));
+        }
+        colorPicker.addView(customColorChooser);
+        customColorChooser.setOnClickListener((View imageView) -> {
+            // when clicked sets the custom color wheel to be visible
+            customColorPicker.setVisibility(View.VISIBLE);
+            brightnessSlideBar.setVisibility(View.VISIBLE);
+            });
+    }
+
     public void selectColor(String newColor) {
         selectedColor = newColor;
         for (int i = 0; i < colors.length; i++) {
             if (colors[i].equals(newColor)) {
+                initCustomColorChooser(); // adds custom color picker, with default color
                 colorPicker.getChildAt(i).performClick();
                 return;
             }
         }
+        // if the board color is not found to be a default color, then a custom color is assumed and
+        // the custom color chooser is setup for this
+        hasCustomColor = true;
+        initCustomColorChooser(); // adds custom color picker, with custom color
     }
 
     public String getSelectedColor() {
