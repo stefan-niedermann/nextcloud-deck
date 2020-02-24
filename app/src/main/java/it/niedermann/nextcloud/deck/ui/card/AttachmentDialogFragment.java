98fae6019cb1dbbd084fa96ddebc74a7d3ed8c90
stefan-niedermann <info@niedermann.it>
2020-02-24 22:02:43 +0100

    Make image viewer background transparent

--- app/src/main/java/it/niedermann/nextcloud/deck/ui/card/AttachmentDialogFragment.java
+++ app/src/main/java/it/niedermann/nextcloud/deck/ui/card/AttachmentDialogFragment.java
@@ -1,15 +1,22 @@
 package it.niedermann.nextcloud.deck.ui.card;
 
 import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
 
 import com.bumptech.glide.Glide;
+
+import java.util.Objects;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -63,6 +70,13 @@ public class AttachmentDialogFragment extends DialogFragment {
         return new AlertDialog.Builder(requireActivity(), Application.getAppTheme(getContext()) ? R.style.DialogDarkTheme : R.style.ThemeOverlay_AppCompat_Dialog_Alert)
                 .setView(view)
                 .create();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Objects.requireNonNull(requireDialog().getWindow()).setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        return super.onCreateView(inflater, container, savedInstanceState);
     }
 
     public static AttachmentDialogFragment newInstance(@NonNull String url, @NonNull String alt) {
