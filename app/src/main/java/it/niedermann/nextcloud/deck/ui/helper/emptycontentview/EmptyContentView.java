c305b607e00ba00ea103c65f72de9290e96d568c
stefan-niedermann <info@niedermann.it>
2020-03-01 16:15:07 +0100

    #285 Migrate from ButterKnife to ViewBinding - EmptyContentView

--- app/src/main/java/it/niedermann/nextcloud/deck/ui/helper/emptycontentview/EmptyContentView.java
+++ app/src/main/java/it/niedermann/nextcloud/deck/ui/helper/emptycontentview/EmptyContentView.java
@@ -6,52 +6,41 @@ import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.StringRes;
-import androidx.appcompat.widget.AppCompatImageView;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
 import it.niedermann.nextcloud.deck.R;
+import it.niedermann.nextcloud.deck.databinding.WidgetEmptyContentViewBinding;
 
 public class EmptyContentView extends RelativeLayout {
 
     private static final int NO_DESCRIPTION = -1;
 
-    @BindView(R.id.title)
-    TextView title;
-    @BindView(R.id.description)
-    TextView description;
-    @BindView(R.id.image)
-    AppCompatImageView image;
+    private WidgetEmptyContentViewBinding binding;
 
     public EmptyContentView(@NonNull Context context, AttributeSet attrs) {
         super(context, attrs);
 
-        LayoutInflater inflater = (LayoutInflater) context
-                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        inflater.inflate(R.layout.widget_empty_content_view, this, true);
-        ButterKnife.bind(this);
-
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        binding = WidgetEmptyContentViewBinding.inflate(inflater, this, true);
 
         TypedArray a = context.obtainStyledAttributes(attrs,
                 R.styleable.EmptyContentView, 0, 0);
 
         @StringRes int descriptionRes = a.getResourceId(R.styleable.EmptyContentView_description, NO_DESCRIPTION);
 
-        title.setText(getResources().getString(a.getResourceId(R.styleable.EmptyContentView_title, R.string.no_content)));
+        binding.title.setText(getResources().getString(a.getResourceId(R.styleable.EmptyContentView_title, R.string.no_content)));
         if (descriptionRes == NO_DESCRIPTION) {
-            description.setVisibility(View.GONE);
+            binding.description.setVisibility(View.GONE);
         } else {
-            description.setText(getResources().getString(descriptionRes));
+            binding.description.setText(getResources().getString(descriptionRes));
         }
-        image.setImageResource(a.getResourceId(R.styleable.EmptyContentView_image, R.drawable.ic_app_logo));
+        binding.image.setImageResource(a.getResourceId(R.styleable.EmptyContentView_image, R.drawable.ic_app_logo));
         a.recycle();
     }
 
     public void hideDescription() {
-        description.setVisibility(View.GONE);
+        binding.description.setVisibility(View.GONE);
     }
 }
