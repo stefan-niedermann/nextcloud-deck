0aac7c87e697d5d994627ee10f5c31159d96c9db
stefan-niedermann <info@niedermann.it>
2020-03-15 19:32:07 +0100

    #248 Add Deck as a "share to" location - refactoring

--- app/src/main/java/it/niedermann/nextcloud/deck/ui/SelectCardActivity.java
+++ app/src/main/java/it/niedermann/nextcloud/deck/ui/SelectCardActivity.java
@@ -5,6 +5,7 @@ import android.content.ClipboardManager;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.net.Uri;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
@@ -13,8 +14,6 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
-
-import com.google.android.material.snackbar.Snackbar;
 
 import java.io.File;
 import java.util.Objects;
@@ -31,60 +30,83 @@ import it.niedermann.nextcloud.deck.util.FileUtils;
 
 public class SelectCardActivity extends MainActivity implements CardAdapter.SelectCardListener {
 
+    Intent receivedIntent;
+    String receivedAction;
+    String receivedType;
+
+    boolean isFile;
+
+    String receivedText;
+    Uri receivedUri;
+    File uploadFile;
+
     @Override
-    public void onCardSelected(FullCard fullCard) {
-        Intent receivedIntent = getIntent();
-        String receivedAction = receivedIntent.getAction();
-        String receivedType = receivedIntent.getType();
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        receivedIntent = getIntent();
+        receivedAction = receivedIntent.getAction();
+        receivedType = receivedIntent.getType();
         DeckLog.info(receivedAction);
         DeckLog.info(receivedType);
         try {
-            if (receivedType == null) {
-                throw new IllegalArgumentException("receivedType must not be null for " + SelectCardActivity.class.getCanonicalName());
-            }
-            if (receivedType.startsWith("text/")) {
-                String receivedText = receivedIntent.getStringExtra(Intent.EXTRA_TEXT);
-                if (receivedText != null) {
-                    appendText(fullCard, receivedText);
-                } else {
-                    DeckLog.warn("Did not receive any text.");
-                }
-            } else if (receivedType.startsWith("image/")) {
-                Uri receivedUri = receivedIntent.getParcelableExtra(Intent.EXTRA_STREAM);
+            isFile = !receivedType.startsWith("text/");
+            if (isFile) {
+                receivedUri = receivedIntent.getParcelableExtra(Intent.EXTRA_STREAM);
                 if (receivedUri != null) {
-                    appendAttachment(fullCard, receivedUri);
+                    String path = FileUtils.getPath(this, receivedUri);
+                    if (path != null) {
+                        uploadFile = new File(path);
+                        binding.toolbar.setSubtitle(uploadFile.getName());
+                    } else {
+                        throw new IllegalArgumentException("Could not find path for given Uri " + receivedUri.toString());
+                    }
                 } else {
-                    DeckLog.warn("Did not receive any extra.");
+                    throw new IllegalArgumentException("Could not find any file for receivedUri = " + receivedUri);
                 }
-                syncManager.updateCard(fullCard);
+            } else {
+                receivedText = receivedIntent.getStringExtra(Intent.EXTRA_TEXT);
+                binding.toolbar.setSubtitle(receivedText);
             }
-            finish();
         } catch (Exception e) {
-            DeckLog.logError(e);
-            String debugInfos = ExceptionUtil.getDebugInfos(this, e);
-            Snackbar.make(binding.coordinatorLayout, R.string.error, Snackbar.LENGTH_LONG)
-                    .setAction(R.string.simple_more, v -> {
-                        AlertDialog dialog = new AlertDialog.Builder(this)
-                                .setTitle(R.string.error)
-                                .setMessage(debugInfos)
-                                .setPositiveButton(android.R.string.copy, (a, b) -> {
-                                    final ClipboardManager clipboardManager = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
-                                    final ClipData clipData = ClipData.newPlainText(e.getMessage(), "```\n" + debugInfos + "\n```");
-                                    Objects.requireNonNull(clipboardManager).setPrimaryClip(clipData);
-                                    Toast.makeText(getApplicationContext(), R.string.copied_to_clipboard, Toast.LENGTH_SHORT).show();
-                                    a.dismiss();
-                                })
-                                .setNegativeButton(R.string.simple_close, null)
-                                .create();
-                        dialog.show();
-                        ((TextView) Objects.requireNonNull(dialog.findViewById(android.R.id.message))).setTypeface(Typeface.MONOSPACE);
-                    })
-                    .show();
+            handleException(e);
         }
     }
 
+    @Override
+    public void onCardSelected(FullCard fullCard) {
+        try {
+            if (isFile) {
+                appendAttachment(fullCard);
+            } else {
+                appendText(fullCard, receivedText);
+            }
+            syncManager.updateCard(fullCard);
+            finish();
+        } catch (Exception e) {
+            handleException(e);
+        }
+    }
+
+    private void handleException(Throwable throwable) {
+        DeckLog.logError(throwable);
+        String debugInfos = ExceptionUtil.getDebugInfos(this, throwable);
+        AlertDialog dialog = new AlertDialog.Builder(this)
+                .setTitle(R.string.error)
+                .setMessage(debugInfos)
+                .setPositiveButton(android.R.string.copy, (a, b) -> {
+                    final ClipboardManager clipboardManager = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+                    final ClipData clipData = ClipData.newPlainText(throwable.getMessage(), "```\n" + debugInfos + "\n```");
+                    Objects.requireNonNull(clipboardManager).setPrimaryClip(clipData);
+                    Toast.makeText(getApplicationContext(), R.string.copied_to_clipboard, Toast.LENGTH_LONG).show();
+                    finish();
+                })
+                .setNegativeButton(R.string.simple_close, (d, w) -> finish())
+                .create();
+        dialog.show();
+        ((TextView) Objects.requireNonNull(dialog.findViewById(android.R.id.message))).setTypeface(Typeface.MONOSPACE);
+    }
+
     private void appendText(@NonNull FullCard fullCard, @NonNull String receivedText) {
-        DeckLog.log(receivedText);
         String oldDescription = fullCard.getCard().getDescription();
         DeckLog.info("Adding to card #" + fullCard.getCard().getId() + " (" + fullCard.getCard().getTitle() + "): Text \"" + receivedText + "\"");
         if (oldDescription == null || oldDescription.length() == 0) {
@@ -92,17 +114,13 @@ public class SelectCardActivity extends MainActivity implements CardAdapter.Sele
         } else {
             fullCard.getCard().setDescription(oldDescription + "\n\n" + receivedText);
         }
+        Toast.makeText(getApplicationContext(), getString(R.string.share_success, "\"" + receivedText + "\"", "\"" + fullCard.getCard().getTitle() + "\""), Toast.LENGTH_LONG).show();
     }
 
-    private void appendAttachment(@NonNull FullCard fullCard, @NonNull Uri uri) {
-        String path = FileUtils.getPath(this, uri);
-        if (path != null) {
-            File uploadFile = new File(path);
-            DeckLog.info("Adding to card #" + fullCard.getCard().getId() + " (" + fullCard.getCard().getTitle() + "): Attachment \"" + uri.toString() + "\"");
-            syncManager.addAttachmentToCard(fullCard.getAccountId(), fullCard.getCard().getLocalId(), Attachment.getMimetypeForUri(this, uri), uploadFile);
-        } else {
-            throw new IllegalArgumentException("Could not find path for given Uri " + uri.toString());
-        }
+    private void appendAttachment(@NonNull FullCard fullCard) {
+        DeckLog.info("Adding to card #" + fullCard.getCard().getId() + " (" + fullCard.getCard().getTitle() + "): Attachment \"" + receivedUri.toString() + "\"");
+        syncManager.addAttachmentToCard(fullCard.getAccountId(), fullCard.getCard().getLocalId(), Attachment.getMimetypeForUri(this, receivedUri), uploadFile);
+        Toast.makeText(getApplicationContext(), getString(R.string.share_success, "\"" + uploadFile.getName() + "\"", "\"" + fullCard.getCard().getTitle() + "\""), Toast.LENGTH_LONG).show();
     }
 
     @Override
