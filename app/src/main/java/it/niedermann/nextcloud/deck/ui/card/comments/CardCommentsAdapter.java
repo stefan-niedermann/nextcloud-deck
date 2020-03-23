7e9b00ea240fa913920885cc3f9b61fb45a89ea1
Stefan Niedermann <info@niedermann.it>
2020-03-23 23:12:33 +0100

    Satisfy code analysis
    Signed-off-by: Stefan Niedermann <info@niedermann.it>

--- app/src/main/java/it/niedermann/nextcloud/deck/ui/card/comments/CardCommentsAdapter.java
+++ app/src/main/java/it/niedermann/nextcloud/deck/ui/card/comments/CardCommentsAdapter.java
@@ -105,6 +105,7 @@ public class CardCommentsAdapter extends RecyclerView.Adapter<CardCommentsAdapte
 
                         @Override
                         public void onLoadCleared(@Nullable Drawable placeholder) {
+                            // silence is gold
                         }
                     });
         }
