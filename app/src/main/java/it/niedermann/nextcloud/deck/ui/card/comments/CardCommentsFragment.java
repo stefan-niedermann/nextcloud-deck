a054d8972223eb8e292dc382c67a88af8d019b31
Stefan Niedermann <info@niedermann.it>
2020-03-23 20:46:12 +0100

    Move Comments fragment into own tab
    Signed-off-by: Stefan Niedermann <info@niedermann.it>

--- app/src/main/java/it/niedermann/nextcloud/deck/ui/card/comments/CardCommentsFragment.java
+++ app/src/main/java/it/niedermann/nextcloud/deck/ui/card/comments/CardCommentsFragment.java
@@ -11,26 +11,25 @@ import androidx.fragment.app.Fragment;
 
 import java.util.Date;
 
-import it.niedermann.nextcloud.deck.databinding.FragmentCardCommentsBinding;
-import it.niedermann.nextcloud.deck.model.Account;
+import it.niedermann.nextcloud.deck.databinding.FragmentCardEditTabCommentsBinding;
 import it.niedermann.nextcloud.deck.model.ocs.comment.DeckComment;
 import it.niedermann.nextcloud.deck.persistence.sync.SyncManager;
 
-import static it.niedermann.nextcloud.deck.ui.card.CardAdapter.BUNDLE_KEY_ACCOUNT;
+import static it.niedermann.nextcloud.deck.ui.card.CardAdapter.BUNDLE_KEY_ACCOUNT_ID;
 import static it.niedermann.nextcloud.deck.ui.card.CardAdapter.BUNDLE_KEY_CAN_EDIT;
 import static it.niedermann.nextcloud.deck.ui.card.CardAdapter.BUNDLE_KEY_LOCAL_ID;
 
 public class CardCommentsFragment extends Fragment {
 
-    private FragmentCardCommentsBinding binding;
+    private FragmentCardEditTabCommentsBinding binding;
 
-    private Account account;
+    private Long accountId;
     private long localId;
     private boolean canEdit = false;
 
-    public static CardCommentsFragment newInstance(Account account, long localId, boolean canEdit) {
+    public static CardCommentsFragment newInstance(long accountId, long localId, boolean canEdit) {
         Bundle bundle = new Bundle();
-        bundle.putSerializable(BUNDLE_KEY_ACCOUNT, account);
+        bundle.putLong(BUNDLE_KEY_ACCOUNT_ID, accountId);
         bundle.putLong(BUNDLE_KEY_LOCAL_ID, localId);
         bundle.putBoolean(BUNDLE_KEY_CAN_EDIT, canEdit);
 
@@ -44,10 +43,10 @@ public class CardCommentsFragment extends Fragment {
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
         Bundle args = getArguments();
-        if (args == null || !args.containsKey(BUNDLE_KEY_ACCOUNT) || !args.containsKey(BUNDLE_KEY_LOCAL_ID)) {
+        if (args == null || !args.containsKey(BUNDLE_KEY_ACCOUNT_ID) || !args.containsKey(BUNDLE_KEY_LOCAL_ID)) {
             throw new IllegalArgumentException("Arguments must at least contain an account and the local card id");
         }
-        this.account = (Account) args.getSerializable(BUNDLE_KEY_ACCOUNT);
+        this.accountId = args.getLong(BUNDLE_KEY_ACCOUNT_ID);
         this.localId = args.getLong(BUNDLE_KEY_LOCAL_ID);
         this.canEdit = args.getBoolean(BUNDLE_KEY_CAN_EDIT, false);
     }
@@ -56,26 +55,27 @@ public class CardCommentsFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container,
                              Bundle savedInstanceState) {
-        binding = FragmentCardCommentsBinding.inflate(inflater, container, false);
-
-        if (canEdit && getActivity() instanceof CommentAddedListener) {
-            binding.addCommentLayout.setVisibility(View.VISIBLE);
-            binding.fab.setOnClickListener(v -> {
-                DeckComment comment = new DeckComment(binding.message.getText().toString());
-                comment.setActorDisplayName(account.getUserName());
-                comment.setCreationDateTime(new Date());
-                ((CommentAddedListener) getActivity()).onCommentAdded(comment);
-                binding.message.setText(null);
-            });
-            binding.message.setOnEditorActionListener((v, actionId, event) -> binding.fab.performClick());
-        } else {
-            binding.addCommentLayout.setVisibility(View.GONE);
-        }
+        binding = FragmentCardEditTabCommentsBinding.inflate(inflater, container, false);
 
         binding.comments.setNestedScrollingEnabled(false);
-        new SyncManager(requireActivity())
-                .getCommentsForLocalCardId(localId).observe(requireActivity(),
-                (comments) -> binding.comments.setAdapter(new CardCommentsAdapter(requireContext(), comments, account)));
+        SyncManager syncManager = new SyncManager(requireActivity());
+        syncManager.readAccount(accountId).observe(requireActivity(), (account -> {
+            syncManager.getCommentsForLocalCardId(localId).observe(requireActivity(),
+                    (comments) -> binding.comments.setAdapter(new CardCommentsAdapter(requireContext(), comments, account)));
+            if (canEdit && getActivity() instanceof CommentAddedListener) {
+                binding.addCommentLayout.setVisibility(View.VISIBLE);
+                binding.fab.setOnClickListener(v -> {
+                    DeckComment comment = new DeckComment(binding.message.getText().toString());
+                    comment.setActorDisplayName(account.getUserName());
+                    comment.setCreationDateTime(new Date());
+                    ((CommentAddedListener) getActivity()).onCommentAdded(comment);
+                    binding.message.setText(null);
+                });
+                binding.message.setOnEditorActionListener((v, actionId, event) -> binding.fab.performClick());
+            } else {
+                binding.addCommentLayout.setVisibility(View.GONE);
+            }
+        }));
         return binding.getRoot();
     }
 }
