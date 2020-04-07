424d9962e31cc8acafeeeade69d68cabce6be290
desperateCoder <echotodevnull@gmail.com>
2020-04-07 11:30:42 +0200

    only sync stacks after reorder

--- app/src/main/java/it/niedermann/nextcloud/deck/persistence/sync/SyncManager.java
+++ app/src/main/java/it/niedermann/nextcloud/deck/persistence/sync/SyncManager.java
@@ -948,13 +948,16 @@ public class SyncManager {
 //            } else {
             reorderLocally(cardsOfNewStack, movedCard, newStackId, newOrder);
             //FIXME: remove the sync-block, when commentblock up there is activated. (waiting for deck server bugfix)
-            synchronize(new IResponseCallback<Boolean>(dataBaseAdapter.getAccountByIdDirectly(accountId)) {
+            Stack stack = dataBaseAdapter.getStackByLocalIdDirectly(movedCard.getCard().getStackId());
+            FullBoard board = dataBaseAdapter.getFullBoardByLocalIdDirectly(accountId, stack.getBoardId());
+            Account account = dataBaseAdapter.getAccountByIdDirectly(movedCard.getCard().getAccountId());
+            new SyncHelper(serverAdapter, dataBaseAdapter, new Date()).setResponseCallback(new IResponseCallback<Boolean>(account) {
                 @Override
                 public void onResponse(Boolean response) {
-
+                    // doNothing();
                 }
-            });
-//            }
+            }).doUpSyncFor(new StackDataProvider(null, board));
+//        }
         });
     }
 
