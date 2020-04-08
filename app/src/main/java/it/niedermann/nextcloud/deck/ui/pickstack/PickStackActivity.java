837f212e2a06fbe083be5a692cc6455eafd92c57
Stefan Niedermann <info@niedermann.it>
2020-04-08 19:14:27 +0200

    #358 Create PickStackActivity
    Signed-off-by: Stefan Niedermann <info@niedermann.it>

---
+++ app/src/main/java/it/niedermann/nextcloud/deck/ui/pickstack/PickStackActivity.java
@@ -0,0 +1,150 @@
+package it.niedermann.nextcloud.deck.ui.pickstack;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
+
+import java.util.List;
+
+import it.niedermann.nextcloud.deck.Application;
+import it.niedermann.nextcloud.deck.DeckLog;
+import it.niedermann.nextcloud.deck.R;
+import it.niedermann.nextcloud.deck.databinding.ActivityPickStackBinding;
+import it.niedermann.nextcloud.deck.model.Account;
+import it.niedermann.nextcloud.deck.model.Board;
+import it.niedermann.nextcloud.deck.model.full.FullStack;
+import it.niedermann.nextcloud.deck.persistence.sync.SyncManager;
+import it.niedermann.nextcloud.deck.ui.ImportAccountActivity;
+import it.niedermann.nextcloud.deck.ui.exception.ExceptionHandler;
+
+import static androidx.lifecycle.Transformations.switchMap;
+
+public class PickStackActivity extends AppCompatActivity {
+
+    private ActivityPickStackBinding binding;
+
+    private SyncManager syncManager;
+
+    private ArrayAdapter<Account> accountAdapter;
+    private ArrayAdapter<Board> boardAdapter;
+    private ArrayAdapter<FullStack> stackAdapter;
+
+    @Nullable
+    private LiveData<List<Board>> boardsLiveData;
+    @NonNull
+    private Observer<List<Board>> boardsObserver = (boards) -> {
+        boardAdapter.clear();
+        boardAdapter.addAll(boards);
+    };
+
+    @Nullable
+    private LiveData<List<FullStack>> stacksLiveData;
+    @NonNull
+    private Observer<List<FullStack>> stacksObserver = (stacks) -> {
+        stackAdapter.clear();
+        stackAdapter.addAll(stacks);
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Thread.setDefaultUncaughtExceptionHandler(new ExceptionHandler(this));
+        setTheme(Application.getAppTheme(this) ? R.style.DarkAppTheme : R.style.AppTheme);
+
+        binding = ActivityPickStackBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setSupportActionBar(binding.toolbar);
+
+        accountAdapter = new AccountAdapter(this);
+        binding.accountSelect.setAdapter(accountAdapter);
+        boardAdapter = new BoardAdapter(this);
+        binding.boardSelect.setAdapter(boardAdapter);
+        stackAdapter = new StackAdapter(this);
+        binding.stackSelect.setAdapter(stackAdapter);
+
+        syncManager = new SyncManager(this);
+
+        switchMap(syncManager.hasAccounts(), hasAccounts -> {
+            if (hasAccounts) {
+                return syncManager.readAccounts();
+            } else {
+                startActivityForResult(new Intent(this, ImportAccountActivity.class), ImportAccountActivity.REQUEST_CODE_IMPORT_ACCOUNT);
+                return null;
+            }
+        }).observe(this, (List<Account> accounts) -> {
+            if (accounts == null) {
+                throw new IllegalStateException("hasAccounts() returns true, but readAccounts() returns null");
+            }
+            accountAdapter.clear();
+            accountAdapter.addAll(accounts);
+            // TODO preselect current account
+            long lastAccountId = Application.readCurrentAccountId(this);
+        });
+
+        binding.accountSelect.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                updateBoardsSource(parent.getSelectedItemId());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Nothing to do here
+            }
+        });
+
+        binding.boardSelect.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Object selectedItem = parent.getSelectedItem();
+                if (selectedItem instanceof Board) {
+                    Board board = (Board) selectedItem;
+                    updateStacksSource(board.getAccountId(), board.getLocalId());
+                } else {
+                    DeckLog.logError(new IllegalArgumentException("parent.getSelectedItem() did not return an instance of " + FullStack.class.getCanonicalName() + " but " + selectedItem.getClass().getCanonicalName()));
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Nothing to do here
+            }
+        });
+        binding.cancel.setOnClickListener((v) -> finish());
+        binding.submit.setOnClickListener((v) -> onSubmit());
+    }
+
+    private void updateBoardsSource(long accountId) {
+        if (boardsLiveData != null) {
+            boardsLiveData.removeObserver(boardsObserver);
+        }
+        boardsLiveData = syncManager.getBoards(accountId);
+        boardsLiveData.observe(this, boardsObserver);
+    }
+
+    private void updateStacksSource(long accountId, long boardId) {
+        if (stacksLiveData != null) {
+            stacksLiveData.removeObserver(stacksObserver);
+        }
+        stacksLiveData = syncManager.getStacksForBoard(accountId, boardId);
+        stacksLiveData.observe(this, stacksObserver);
+    }
+
+    private void onSubmit() {
+        // Start EditActivity and pass parameters
+        long accountId = binding.accountSelect.getSelectedItemId();
+        long boardId = binding.boardSelect.getSelectedItemId();
+        long stackId = binding.stackSelect.getSelectedItemId();
+
+
+    }
+}
---
+++ app/src/main/java/it/niedermann/nextcloud/deck/ui/pickstack/StackAdapter.java
@@ -0,0 +1,66 @@
+package it.niedermann.nextcloud.deck.ui.pickstack;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+import it.niedermann.nextcloud.deck.DeckLog;
+import it.niedermann.nextcloud.deck.R;
+import it.niedermann.nextcloud.deck.databinding.ItemPickStackStackBinding;
+import it.niedermann.nextcloud.deck.model.full.FullStack;
+
+public class StackAdapter extends ArrayAdapter<FullStack> {
+
+    @NonNull
+    private final LayoutInflater inflater;
+
+    @SuppressWarnings("WeakerAccess")
+    public StackAdapter(@NonNull Context context) {
+        super(context, R.layout.item_pick_stack_account);
+        setDropDownViewResource(R.layout.item_pick_stack_account);
+        inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return Objects.requireNonNull(getItem(position)).getLocalId();
+    }
+
+    @NotNull
+    @Override
+    public View getView(int position, View convertView, @NotNull ViewGroup parent) {
+        final ItemPickStackStackBinding binding;
+        if (convertView == null) {
+            binding = ItemPickStackStackBinding.inflate(inflater, parent, false);
+        } else {
+            binding = ItemPickStackStackBinding.bind(convertView);
+        }
+
+        final FullStack item = getItem(position);
+        if (item != null) {
+            binding.stackTitle.setText(item.getStack().getTitle());
+        } else {
+            DeckLog.logError(new IllegalArgumentException("No item for position " + position));
+        }
+        return binding.getRoot();
+    }
+
+    @Override
+    public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        return getView(position, convertView, parent);
+    }
+}
