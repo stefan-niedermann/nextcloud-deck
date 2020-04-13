e2bf80625054b573f17eac6e678ab986c8cd5bc5
Stefan Niedermann <info@niedermann.it>
2020-04-13 17:48:44 +0200

    #208 Handle push notifications
    Display progressBar with fallback to browser
    
    Signed-off-by: Stefan Niedermann <info@niedermann.it>

--- app/src/main/java/it/niedermann/nextcloud/deck/ui/PushNotificationActivity.java
+++ app/src/main/java/it/niedermann/nextcloud/deck/ui/PushNotificationActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 import it.niedermann.nextcloud.deck.DeckLog;
+import it.niedermann.nextcloud.deck.R;
 import it.niedermann.nextcloud.deck.api.IResponseCallback;
 import it.niedermann.nextcloud.deck.databinding.ActivityPushNotificationBinding;
 import it.niedermann.nextcloud.deck.model.full.FullCard;
@@ -20,6 +21,8 @@ import static it.niedermann.nextcloud.deck.ui.card.CardAdapter.BUNDLE_KEY_BOARD_
 import static it.niedermann.nextcloud.deck.ui.card.CardAdapter.BUNDLE_KEY_LOCAL_ID;
 
 public class PushNotificationActivity extends AppCompatActivity {
+
+    private ActivityPushNotificationBinding binding;
 
     // Provided by Files app NotificationJob
     private static final String KEY_SUBJECT = "subject";
@@ -39,35 +42,8 @@ public class PushNotificationActivity extends AppCompatActivity {
             throw new IllegalArgumentException("Could not retrieve intent");
         }
 
-        final ActivityPushNotificationBinding binding = ActivityPushNotificationBinding.inflate(getLayoutInflater());
+        binding = ActivityPushNotificationBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-        final SyncManager syncManager = new SyncManager(this);
-        final String cardRemoteIdString = getIntent().getStringExtra(KEY_CARD_REMOTE_ID);
-        final String accountString = getIntent().getStringExtra(KEY_ACCOUNT);
-
-        DeckLog.info("[PUSH] " + "cardRemoteIdString = "+ cardRemoteIdString);
-        if (cardRemoteIdString != null) {
-            try {
-                final int cardRemoteId = Integer.parseInt(cardRemoteIdString);
-                syncManager.readAccount(accountString).observe(this, account -> {
-                    DeckLog.info("[PUSH] " + "account: " + account);
-                    syncManager.synchronizeCardByRemoteId(cardRemoteId, new IResponseCallback<FullCard>(account) {
-                        @Override
-                        public void onResponse(FullCard response) {
-                            DeckLog.info("[PUSH] " + "FullCard: " + response);
-                            syncManager.getLocalBoardIdByCardRemoteIdAndAccount(cardRemoteId, account).observe(PushNotificationActivity.this, boardLocalId -> {
-                                DeckLog.info("[PUSH] " + "BoardLocalId " + boardLocalId);
-                                launchEditActivity(account.getId(), response.getLocalId(), boardLocalId);
-                            });
-                        }
-                    });
-                });
-            } catch (NumberFormatException e) {
-                // card id cannot be read, fallback to default
-                DeckLog.logError(e);
-            }
-        }
 
         binding.subject.setText(getIntent().getStringExtra(KEY_SUBJECT));
 
@@ -78,26 +54,92 @@ public class PushNotificationActivity extends AppCompatActivity {
         }
 
         final String link = getIntent().getStringExtra(KEY_LINK);
-        if (link != null) {
-            binding.submit.setOnClickListener((v) -> {
-                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(link));
-                startActivity(browserIntent);
-            });
-        } else {
-            binding.submit.setEnabled(false);
-        }
 
         binding.cancel.setOnClickListener((v) -> finish());
+
+        final SyncManager syncManager = new SyncManager(this);
+        final String cardRemoteIdString = getIntent().getStringExtra(KEY_CARD_REMOTE_ID);
+        final String accountString = getIntent().getStringExtra(KEY_ACCOUNT);
+
+        DeckLog.verbose("cardRemoteIdString = " + cardRemoteIdString);
+        if (cardRemoteIdString != null) {
+            try {
+                final int cardRemoteId = Integer.parseInt(cardRemoteIdString);
+                syncManager.readAccount(accountString).observe(this, account -> {
+                    if (account != null) {
+                        DeckLog.verbose("account: " + account);
+                        syncManager.synchronizeCardByRemoteId(cardRemoteId, new IResponseCallback<FullCard>(account) {
+                            @Override
+                            public void onResponse(FullCard response) {
+                                DeckLog.verbose("FullCard: " + response);
+                                runOnUiThread(() -> syncManager.getLocalBoardIdByCardRemoteIdAndAccount(cardRemoteId, account).observe(PushNotificationActivity.this, boardLocalId -> {
+                                    if (boardLocalId != null) {
+                                        DeckLog.verbose("BoardLocalId " + boardLocalId);
+                                        binding.submit.setOnClickListener((v) -> launchEditActivity(account.getId(), response.getLocalId(), boardLocalId));
+                                        binding.submit.setEnabled(true);
+                                        binding.progress.setVisibility(View.INVISIBLE);
+                                    } else {
+                                        fallbackToBrowser(link);
+                                    }
+                                }));
+                            }
+
+                            @Override
+                            public void onError(Throwable throwable) {
+                                super.onError(throwable);
+                                fallbackToBrowser(link);
+                            }
+                        });
+                    } else {
+                        fallbackToBrowser(link);
+                    }
+                });
+            } catch (NumberFormatException e) {
+                // card id cannot be read, fallback to default
+                DeckLog.logError(e);
+                fallbackToBrowser(link);
+            }
+        } else {
+            fallbackToBrowser(link);
+        }
+    }
+
+    /**
+     * If anything goes wrong and we cannot open the card directly, we fall back to open the given link in the webbrowser
+     */
+    private void fallbackToBrowser(String link) {
+        runOnUiThread(() -> {
+            try {
+                final Uri uri = Uri.parse(link);
+                binding.submit.setOnClickListener((v) -> {
+                    Intent browserIntent = new Intent(Intent.ACTION_VIEW, uri);
+                    startActivity(browserIntent);
+                });
+                binding.submit.setText(R.string.open_in_browser);
+                binding.submit.setEnabled(true);
+                binding.progress.setVisibility(View.INVISIBLE);
+            } catch (Throwable t) {
+                DeckLog.logError(t);
+            }
+        });
     }
 
     private void launchEditActivity(Long accountId, Long cardId, Long boardId) {
-        DeckLog.info("[PUSH] " + "starting activity with [" + accountId + ", " + cardId + ", " + boardId + "]");
-        Intent intent = new Intent(this, EditActivity.class)
-                .putExtra(BUNDLE_KEY_ACCOUNT_ID, accountId)
-                .putExtra(BUNDLE_KEY_LOCAL_ID, cardId)
-                .putExtra(BUNDLE_KEY_BOARD_ID, boardId)
-                .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
-        finish();
+        DeckLog.info("starting activity with [" + accountId + ", " + cardId + ", " + boardId + "]");
+        runOnUiThread(() -> {
+            Intent intent = new Intent(this, EditActivity.class)
+                    .putExtra(BUNDLE_KEY_ACCOUNT_ID, accountId)
+                    .putExtra(BUNDLE_KEY_LOCAL_ID, cardId)
+                    .putExtra(BUNDLE_KEY_BOARD_ID, boardId)
+                    .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+            finish();
+        });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish(); // close this activity as oppose to navigating up
+        return true;
     }
 }
