6ba9ee95e625ba6c1ab71526fbc5de0abcba8b44
Stefan Niedermann <info@niedermann.it>
2020-04-14 13:32:16 +0200

    Remove obsolete code
    Signed-off-by: Stefan Niedermann <info@niedermann.it>

--- app/src/main/java/it/niedermann/nextcloud/deck/ui/board/accesscontrol/AccessControlViewHolder.java
+++ app/src/main/java/it/niedermann/nextcloud/deck/ui/board/accesscontrol/AccessControlViewHolder.java
@@ -5,8 +5,6 @@ import androidx.recyclerview.widget.RecyclerView;
 import it.niedermann.nextcloud.deck.databinding.ItemAccessControlBinding;
 
 public class AccessControlViewHolder extends RecyclerView.ViewHolder {
-    // @BindDrawable(R.drawable.ic_sync_blue_24dp)
-    // Drawable syncIcon;
     public ItemAccessControlBinding binding;
 
     @SuppressWarnings("WeakerAccess")
