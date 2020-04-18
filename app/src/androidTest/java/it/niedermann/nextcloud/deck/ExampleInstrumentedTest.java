067c24b9c37f2fa376d71ff7fc4c50a85165a594
Niedermann IT-Dienstleistungen <stefan-niedermann@users.noreply.github.com>
2020-04-18 13:20:29 +0200

    Instrumented tests ci (#386)
    * Trial and error - add instrumented tests to CI
    
    * Trial and error - add instrumented tests to CI
    
    Setup AVD
    
    * Trial and error - add instrumented tests to CI
    
    Fix action version
    
    * Try API level 27 for AVD
    
    https://github.com/ReactiveCircus/android-emulator-runner/issues/45
    
    * Try API level 29 for AVD on macos-latest image
    
    https://github.com/ReactiveCircus/android-emulator-runner/issues/45
    
    * Explicitly watch instrumented test fail
    
    * Fix instrumented tests and prepare merge into master

--- app/src/androidTest/java/it/niedermann/nextcloud/deck/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/it/niedermann/nextcloud/deck/ExampleInstrumentedTest.java
@@ -1,8 +1,9 @@
 package it.niedermann.nextcloud.deck;
 
 import android.content.Context;
-import androidx.test.InstrumentationRegistry;
-import androidx.test.runner.AndroidJUnit4;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,8 +20,8 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("it.niedermann.nextcloud.deck", appContext.getPackageName());
+        assertEquals("it.niedermann.nextcloud.deck.dev", appContext.getPackageName());
     }
 }
---
+++ app/src/androidTest/java/it/niedermann/nextcloud/deck/util/ColorUtilTest.java
@@ -0,0 +1,33 @@
+package it.niedermann.nextcloud.deck.util;
+
+import android.graphics.Color;
+
+import androidx.annotation.ColorInt;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ColorUtilTest {
+
+    @Test
+    public void testIsColorDark() {
+        @ColorInt int[] darkColors = new int[]{Color.BLACK};
+        @ColorInt int[] lightColors = new int[]{Color.WHITE};
+        for (@ColorInt int darkColor : darkColors) {
+            assertTrue(ColorUtil.isColorDark(darkColor));
+        }
+        for (@ColorInt int lightColor : lightColors) {
+            assertFalse(ColorUtil.isColorDark(lightColor));
+        }
+    }
+}
