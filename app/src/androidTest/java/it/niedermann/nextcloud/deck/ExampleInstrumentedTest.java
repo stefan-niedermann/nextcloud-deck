c78161640797bf63a35c2cf61615e68fcdcaeac7
Stefan Niedermann <info@niedermann.it>
2020-04-18 13:14:09 +0200

    Fix instrumented tests and prepare merge into master

--- app/src/androidTest/java/it/niedermann/nextcloud/deck/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/it/niedermann/nextcloud/deck/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("it.niedermann.nextcloud.deck", appContext.getPackageName());
+        assertEquals("it.niedermann.nextcloud.deck.dev", appContext.getPackageName());
     }
 }
