b953c16bfb57dcfc4e8b4702125bb0a9125bece8
Stefan Niedermann <info@niedermann.it>
2020-04-23 19:00:59 +0200

    #396  Filter cards
    RecyclerView with selection - working draft

--- app/src/main/java/it/niedermann/nextcloud/deck/ui/filter/LabelFilterAdapter.java
+++ app/src/main/java/it/niedermann/nextcloud/deck/ui/filter/LabelFilterAdapter.java
@@ -4,68 +4,81 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
 
-import org.jetbrains.annotations.NotNull;
+import java.util.List;
 
-import java.util.NoSuchElementException;
-
+import it.niedermann.nextcloud.deck.R;
 import it.niedermann.nextcloud.deck.model.Label;
 
-public class LabelFilterAdapter extends ArrayAdapter<Label> {
+public class LabelFilterAdapter extends RecyclerView.Adapter<LabelFilterAdapter.LabelViewHolder> {
+    private Context context;
+    private List<Label> labels;
+    private List<Long> selectedLabelIds;
 
-    @NonNull
-    private final LayoutInflater inflater;
-
-    @SuppressWarnings("WeakerAccess")
-    public LabelFilterAdapter(@NonNull Context context) {
-        super(context, android.R.layout.simple_list_item_multiple_choice, android.R.id.text1);
-        inflater = LayoutInflater.from(context);
-    }
-
-    @Override
-    public boolean hasStableIds() {
-        return true;
+    public LabelFilterAdapter(@NonNull Context context, @NonNull List<Label> labels, @NonNull List<Long> selectedLabelIds) {
+        super();
+        this.context = context;
+        this.labels = labels;
+        this.selectedLabelIds = selectedLabelIds;
+        setHasStableIds(true);
+        notifyDataSetChanged();
     }
 
     @Override
     public long getItemId(int position) {
-        final Label label = getItem(position);
-        if (label == null) {
-            throw new NoSuchElementException();
-        }
-        return label.getLocalId();
+        return labels.get(position).getLocalId();
+    }
+
+    @NonNull
+    @Override
+    public LabelViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_user, viewGroup, false);
+        return new LabelViewHolder(view);
     }
 
     @Override
-    public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        return getView(position, convertView, parent);
+    public void onBindViewHolder(@NonNull LabelViewHolder multiViewHolder, int position) {
+        multiViewHolder.bind(labels.get(position));
     }
 
-    @SuppressWarnings("WeakerAccess")
-    public int getPosition(long labelId) {
-        for (int i = 0; i < getCount(); i++) {
-            if (getItemId(i) == labelId) {
-                return i;
-            }
-        }
-        throw new NoSuchElementException();
-    }
-
-    @NotNull
     @Override
-    public View getView(int position, View convertView, @NotNull ViewGroup parent) {
-        final View view;
-        if (convertView == null) {
-            view = inflater.inflate(android.R.layout.simple_list_item_multiple_choice, parent, false);
-        } else {
-            view = convertView;
+    public int getItemCount() {
+        return labels.size();
+    }
+
+    public List<Long> getSelected() {
+        return selectedLabelIds;
+    }
+
+    class LabelViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView textView;
+        private ImageView imageView;
+
+        LabelViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.displayname);
+            imageView = itemView.findViewById(R.id.avatar);
         }
-        ((TextView) view.findViewById(android.R.id.text1)).setText(getItem(position).getTitle());
-        return view;
+
+        void bind(final Label label) {
+            imageView.setVisibility(selectedLabelIds.contains(label.getLocalId()) ? View.VISIBLE : View.GONE);
+            textView.setText(label.getTitle());
+
+            itemView.setOnClickListener(view -> {
+                if (selectedLabelIds.contains(label.getLocalId())) {
+                    selectedLabelIds.remove(label.getLocalId());
+                    itemView.setSelected(false);
+                } else {
+                    selectedLabelIds.add(label.getLocalId());
+                    itemView.setSelected(true);
+                }
+            });
+        }
     }
 }
