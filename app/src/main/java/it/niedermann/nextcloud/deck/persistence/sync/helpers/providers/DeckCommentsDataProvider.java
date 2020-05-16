dc3d051ed0dbac35d4d5a60b16a06eae52bebf19
Stefan Niedermann <info@niedermann.it>
2020-05-16 14:54:05 +0200

    Use super instead of direct logging in onError callback

--- app/src/main/java/it/niedermann/nextcloud/deck/persistence/sync/helpers/providers/DeckCommentsDataProvider.java
+++ app/src/main/java/it/niedermann/nextcloud/deck/persistence/sync/helpers/providers/DeckCommentsDataProvider.java
@@ -75,7 +75,7 @@ public class DeckCommentsDataProvider extends AbstractSyncDataProvider<OcsCommen
 
                     @Override
                     public void onError(Throwable throwable) {
-                        DeckLog.logError(throwable);
+                        super.onError(throwable);
                         throw new IllegalStateException("could not find parent comment with id "+comment.getParentId()+" on server.", throwable);
                     }
                 }, null);
