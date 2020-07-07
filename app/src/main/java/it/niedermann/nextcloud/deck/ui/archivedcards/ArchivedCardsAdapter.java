2bf6cd3860e042ec4b6c829d507532ea58aaea46
Stefan Niedermann <info@niedermann.it>
2020-07-07 12:03:55 +0200

    Move binding for CardViewHolder into CardViewHolder

--- app/src/main/java/it/niedermann/nextcloud/deck/ui/archivedcards/ArchivedCardsAdapter.java
+++ app/src/main/java/it/niedermann/nextcloud/deck/ui/archivedcards/ArchivedCardsAdapter.java
@@ -11,33 +11,44 @@ import it.niedermann.nextcloud.deck.R;
 import it.niedermann.nextcloud.deck.model.Account;
 import it.niedermann.nextcloud.deck.model.full.FullCard;
 import it.niedermann.nextcloud.deck.persistence.sync.SyncManager;
+import it.niedermann.nextcloud.deck.persistence.sync.adapters.db.util.WrappedLiveData;
 import it.niedermann.nextcloud.deck.ui.card.CardAdapter;
 import it.niedermann.nextcloud.deck.ui.card.CardViewHolder;
+import it.niedermann.nextcloud.deck.ui.exception.ExceptionDialogFragment;
+
+import static it.niedermann.nextcloud.deck.persistence.sync.adapters.db.util.LiveDataHelper.observeOnce;
 
 public class ArchivedCardsAdapter extends CardAdapter {
 
     @SuppressWarnings("WeakerAccess")
-    public ArchivedCardsAdapter(@NonNull Context context, @NonNull FragmentManager fragmentManager, @NonNull Account account, long boardId, boolean canEdit, @NonNull SyncManager syncManager, @NonNull LifecycleOwner lifecycleOwner) {
-        super(context, fragmentManager, account, boardId, 0L, 0L, canEdit, syncManager, lifecycleOwner, null);
+    public ArchivedCardsAdapter(@NonNull Context context, @NonNull FragmentManager fragmentManager, @NonNull Account account, long boardId, boolean hasEditPermission, @NonNull SyncManager syncManager, @NonNull LifecycleOwner lifecycleOwner) {
+        super(context, fragmentManager, account, boardId, 0L, 0L, hasEditPermission, syncManager, lifecycleOwner, null);
     }
 
     @Override
     public void onBindViewHolder(@NonNull CardViewHolder viewHolder, int position) {
-        viewHolder.bind(cardList.get(position), this, position, account, boardLocalId, boardRemoteId, hasEditPermission, fullCard -> {
-        }, R.menu.archived_card_menu, this, counterMaxValue, mainColor);
+        viewHolder.bind(cardList.get(position), account, boardRemoteId, hasEditPermission, R.menu.archived_card_menu, this, counterMaxValue, mainColor);
     }
 
     @Override
     public boolean onCardOptionsItemSelected(@NonNull MenuItem menuItem, @NonNull FullCard fullCard) {
         switch (menuItem.getItemId()) {
             case R.id.action_card_dearchive: {
-                // TODO error handling
-                new Thread(() -> syncManager.dearchiveCard(fullCard)).start();
+                final WrappedLiveData<FullCard> liveData = syncManager.dearchiveCard(fullCard);
+                observeOnce(liveData, lifecycleOwner, (next) -> {
+                    if (liveData.hasError()) {
+                        ExceptionDialogFragment.newInstance(liveData.getError(), account).show(fragmentManager, ExceptionDialogFragment.class.getSimpleName());
+                    }
+                });
                 return true;
             }
             case R.id.action_card_delete: {
-                // TODO error handling
-                syncManager.deleteCard(fullCard.getCard());
+                final WrappedLiveData<Void> liveData = syncManager.deleteCard(fullCard.getCard());
+                observeOnce(liveData, lifecycleOwner, (next) -> {
+                    if (liveData.hasError()) {
+                        ExceptionDialogFragment.newInstance(liveData.getError(), account).show(fragmentManager, ExceptionDialogFragment.class.getSimpleName());
+                    }
+                });
                 return true;
             }
             default: {
--- app/src/main/java/it/niedermann/nextcloud/deck/ui/card/CardViewHolder.java
+++ app/src/main/java/it/niedermann/nextcloud/deck/ui/card/CardViewHolder.java
@@ -1,10 +1,10 @@
 package it.niedermann.nextcloud.deck.ui.card;
 
-import android.content.ClipData;
 import android.content.Context;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnLongClickListener;
 import android.widget.TextView;
 
 import androidx.annotation.ColorInt;
@@ -15,12 +15,12 @@ import androidx.appcompat.widget.PopupMenu;
 import androidx.core.graphics.drawable.DrawableCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.card.MaterialCardView;
+
 import org.jetbrains.annotations.Contract;
 
 import java.util.List;
 
-import it.niedermann.android.crosstabdnd.DraggedItemLocalState;
-import it.niedermann.nextcloud.deck.DeckLog;
 import it.niedermann.nextcloud.deck.R;
 import it.niedermann.nextcloud.deck.databinding.ItemCardBinding;
 import it.niedermann.nextcloud.deck.model.Account;
@@ -36,33 +36,20 @@ public class CardViewHolder extends RecyclerView.ViewHolder {
     private ItemCardBinding binding;
 
     @SuppressWarnings("WeakerAccess")
-    public CardViewHolder(ItemCardBinding binding) {
+    public CardViewHolder(@NonNull ItemCardBinding binding) {
         super(binding.getRoot());
         this.binding = binding;
     }
 
-    public void bind(@NonNull FullCard fullCard, @NonNull CardAdapter cardAdapter, int position, @NonNull Account account, long boardLocalId, @Nullable Long boardRemoteId, boolean hasEditPermission, @Nullable SelectCardListener selectCardListener, @MenuRes int optionsMenu, @NonNull CardOptionsItemSelectedListener optionsItemsSelectedListener, @NonNull String counterMaxValue, @ColorInt int mainColor) {
+    /**
+     * Removes all {@link OnClickListener} and {@link OnLongClickListener}
+     */
+    public void bind(@NonNull FullCard fullCard, @NonNull Account account, @Nullable Long boardRemoteId, boolean hasEditPermission, @MenuRes int optionsMenu, @NonNull CardOptionsItemSelectedListener optionsItemsSelectedListener, @NonNull String counterMaxValue, @ColorInt int mainColor) {
         final Context context = itemView.getContext();
-        binding.card.setOnClickListener((v) -> {
-            if (selectCardListener == null) {
-                context.startActivity(EditActivity.createEditCardIntent(context, account, boardLocalId, fullCard.getLocalId()));
-            } else {
-                selectCardListener.onCardSelected(fullCard);
-            }
-        });
-        if (hasEditPermission && selectCardListener == null) {
-            binding.card.setOnLongClickListener((v) -> {
-                DeckLog.log("Starting drag and drop");
-                v.startDrag(ClipData.newPlainText("cardid", String.valueOf(fullCard.getLocalId())),
-                        new View.DragShadowBuilder(v),
-                        new DraggedItemLocalState<>(fullCard, binding.card, cardAdapter, position),
-                        0
-                );
-                return true;
-            });
-        } else {
-            binding.cardMenu.setVisibility(View.GONE);
-        }
+
+        bindCardClickListener(null);
+        bindCardLongClickListener(null);
+        binding.cardMenu.setVisibility(hasEditPermission ? View.VISIBLE : View.GONE);
         binding.cardTitle.setText(fullCard.getCard().getTitle().trim());
 
         if (fullCard.getAssignedUsers() != null && fullCard.getAssignedUsers().size() > 0) {
@@ -118,27 +105,49 @@ public class CardViewHolder extends RecyclerView.ViewHolder {
             binding.cardCountTasks.setVisibility(View.GONE);
         }
 
-        binding.cardMenu.setOnClickListener(v -> onOverflowIconClicked(optionsMenu, v, fullCard, account, boardRemoteId, optionsItemsSelectedListener));
+        binding.cardMenu.setOnClickListener(view -> {
+            final PopupMenu popup = new PopupMenu(context, view);
+            popup.inflate(optionsMenu);
+            final Menu menu = popup.getMenu();
+            if (containsUser(fullCard.getAssignedUsers(), account.getUserName())) {
+                menu.removeItem(menu.findItem(R.id.action_card_assign).getItemId());
+            } else {
+                menu.removeItem(menu.findItem(R.id.action_card_unassign).getItemId());
+            }
+            if (boardRemoteId == null || fullCard.getCard().getId() == null) {
+                menu.removeItem(R.id.share_link);
+            }
+
+            popup.setOnMenuItemClickListener(item -> optionsItemsSelectedListener.onCardOptionsItemSelected(item, fullCard));
+            popup.show();
+        });
     }
 
-    private void onOverflowIconClicked(@MenuRes int optionsMenu, @NonNull View view, FullCard fullCard, @NonNull Account account, @Nullable Long boardRemoteId, @NonNull CardOptionsItemSelectedListener optionsItemsSelectedListener) {
-        final Context context = view.getContext();
-        final PopupMenu popup = new PopupMenu(context, view);
-        popup.inflate(optionsMenu);
-        prepareOptionsMenu(popup.getMenu(), fullCard, account, boardRemoteId);
-
-        popup.setOnMenuItemClickListener(item -> optionsItemsSelectedListener.onCardOptionsItemSelected(item, fullCard));
-        popup.show();
+    public void bindCardClickListener(OnClickListener l) {
+        binding.card.setOnClickListener(l);
     }
 
-    private void prepareOptionsMenu(Menu menu, @NonNull FullCard card, @NonNull Account account, @Nullable Long boardRemoteId) {
-        if (containsUser(card.getAssignedUsers(), account.getUserName())) {
-            menu.removeItem(menu.findItem(R.id.action_card_assign).getItemId());
-        } else {
-            menu.removeItem(menu.findItem(R.id.action_card_unassign).getItemId());
-        }
-        if (boardRemoteId == null || card.getCard().getId() == null) {
-            menu.removeItem(R.id.share_link);
+    public void bindCardLongClickListener(OnLongClickListener l) {
+        binding.card.setOnLongClickListener(l);
+    }
+
+    public MaterialCardView getDraggable() {
+        return binding.card;
+    }
+
+    private static void setupDueDate(@NonNull TextView cardDueDate, @NonNull Card card) {
+        final Context context = cardDueDate.getContext();
+        cardDueDate.setText(DateUtil.getRelativeDateTimeString(context, card.getDueDate().getTime()));
+        ViewUtil.themeDueDate(context, cardDueDate, card.getDueDate());
+    }
+
+    private static void setupCounter(@NonNull TextView textView, @NonNull String counterMaxValue, int count) {
+        if (count > 99) {
+            textView.setText(counterMaxValue);
+        } else if (count > 1) {
+            textView.setText(String.valueOf(count));
+        } else if (count == 1) {
+            textView.setText("");
         }
     }
 
@@ -153,25 +162,4 @@ public class CardViewHolder extends RecyclerView.ViewHolder {
         }
         return false;
     }
-
-    private void setupCounter(@NonNull TextView textView, @NonNull String counterMaxValue, int count) {
-        if (count > 99) {
-            textView.setText(counterMaxValue);
-        } else if (count > 1) {
-            textView.setText(String.valueOf(count));
-        } else if (count == 1) {
-            textView.setText("");
-        }
-    }
-
-    private void setupDueDate(@NonNull TextView cardDueDate, @NonNull Card card) {
-        final Context context = cardDueDate.getContext();
-        cardDueDate.setText(DateUtil.getRelativeDateTimeString(context, card.getDueDate().getTime()));
-        ViewUtil.themeDueDate(context, cardDueDate, card.getDueDate());
-    }
-
-    public interface CardOptionsItemSelectedListener {
-        boolean onCardOptionsItemSelected(@NonNull MenuItem menuItem, @NonNull FullCard fullCard);
-    }
-
 }
