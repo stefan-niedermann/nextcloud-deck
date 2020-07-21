68f68f4f9696dbb2341d2b1c8ef40d6c32c26e89
desperateCoder <echotodevnull@gmail.com>
2020-07-21 12:24:44 +0200

    #573 Projects: i said nothing to see here!!11!

--- app/src/main/java/it/niedermann/nextcloud/deck/api/JsonToEntityParser.java
+++ app/src/main/java/it/niedermann/nextcloud/deck/api/JsonToEntityParser.java
@@ -149,7 +149,7 @@ public class JsonToEntityParser {
                resource.setMimetype(obj.get("mimetype").getAsString());
             }
             if (obj.has("preview-available")) {
-               resource.setPreviewAvailable(obj.get("preview-availableath").getAsBoolean());
+               resource.setPreviewAvailable(obj.get("preview-available").getAsBoolean());
             } else {
                resource.setPreviewAvailable(false);
             }
