f72138d30509322ac22643a137beaa5a89e9d682
Stefan Niedermann <info@niedermann.it>
2020-07-22 16:12:00 +0200

    #573 projects - Enhance resources dialog

--- app/src/main/java/it/niedermann/nextcloud/deck/ui/card/projectresources/CardProjectResourcesDialog.java
+++ app/src/main/java/it/niedermann/nextcloud/deck/ui/card/projectresources/CardProjectResourcesDialog.java
@@ -1,13 +1,13 @@
 package it.niedermann.nextcloud.deck.ui.card.projectresources;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -15,9 +15,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-import it.niedermann.nextcloud.deck.DeckLog;
 import it.niedermann.nextcloud.deck.databinding.DialogProjectResourcesBinding;
 import it.niedermann.nextcloud.deck.model.ocs.projects.OcsProjectResource;
+import it.niedermann.nextcloud.deck.ui.branding.BrandedAlertDialogBuilder;
 import it.niedermann.nextcloud.deck.ui.branding.BrandedDialogFragment;
 import it.niedermann.nextcloud.deck.ui.card.EditCardViewModel;
 
@@ -42,24 +42,21 @@ public class CardProjectResourcesDialog extends BrandedDialogFragment {
         this.projectName = args.getString(KEY_PROJECT_NAME);
     }
 
+    @NonNull
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        final DialogProjectResourcesBinding binding = DialogProjectResourcesBinding.inflate(inflater, container, false);
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final DialogProjectResourcesBinding binding = DialogProjectResourcesBinding.inflate(LayoutInflater.from(requireContext()));
         final EditCardViewModel viewModel = new ViewModelProvider(requireActivity()).get(EditCardViewModel.class);
-
-        // TODO check if necessary
-        // This might be a zombie fragment with an empty EditCardViewModel after Android killed the activity (but not the fragment instance
-        // See https://github.com/stefan-niedermann/nextcloud-deck/issues/478
-        if (viewModel.getFullCard() == null) {
-            DeckLog.logError(new IllegalStateException("Cannot populate " + CardProjectResourcesDialog.class.getSimpleName() + " because viewModel.getFullCard() is null"));
-            return binding.getRoot();
-        }
-
         final CardProjectResourceAdapter adapter = new CardProjectResourceAdapter(viewModel.getAccount(), resources);
 
-        binding.projectName.setText(projectName);
-        binding.recyclerView.setAdapter(adapter);
-        return binding.getRoot();
+        AlertDialog.Builder dialogBuilder = new BrandedAlertDialogBuilder(requireContext());
+
+        binding.getRoot().setAdapter(adapter);
+        return dialogBuilder
+                .setTitle(projectName)
+                .setView(binding.getRoot())
+                .setNeutralButton(android.R.string.cancel, null)
+                .create();
     }
 
     @Override
