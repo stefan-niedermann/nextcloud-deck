89bc28d6f0fd02e128a7c6c5498c48d4f87d13b1
Stefan Niedermann <info@niedermann.it>
2020-09-04 16:51:06 +0200

    #673 💬 Allow mention users in the comments
    Signed-off-by: Stefan Niedermann <info@niedermann.it>

--- app/src/main/java/it/niedermann/nextcloud/deck/ui/card/comments/CardCommentsMentionProposer.java
+++ app/src/main/java/it/niedermann/nextcloud/deck/ui/card/comments/CardCommentsMentionProposer.java
@@ -16,13 +16,16 @@ import androidx.lifecycle.LifecycleOwner;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import it.niedermann.nextcloud.deck.R;
 import it.niedermann.nextcloud.deck.model.Account;
 import it.niedermann.nextcloud.deck.model.User;
 import it.niedermann.nextcloud.deck.persistence.sync.SyncManager;
-import it.niedermann.nextcloud.deck.persistence.sync.adapters.db.util.LiveDataHelper;
 import it.niedermann.nextcloud.deck.ui.card.comments.util.CommentsUtil;
 
+import static it.niedermann.nextcloud.deck.persistence.sync.adapters.db.util.LiveDataHelper.observeOnce;
 import static it.niedermann.nextcloud.deck.util.DimensionUtil.dpToPx;
 
 public class CardCommentsMentionProposer implements TextWatcher {
@@ -41,6 +44,9 @@ public class CardCommentsMentionProposer implements TextWatcher {
     private final EditText editText;
     @NonNull
     private final LinearLayout mentionProposerLayout;
+
+    @NonNull
+    private final List<User> users = new ArrayList<>();
 
     public CardCommentsMentionProposer(@NonNull LifecycleOwner owner, @NonNull Account account, long boardLocalId, @NonNull EditText editText, @NonNull LinearLayout avatarProposerLayout) {
         this.owner = owner;
@@ -64,32 +70,43 @@ public class CardCommentsMentionProposer implements TextWatcher {
     public void onTextChanged(CharSequence s, int start, int before, int count) {
         final int selectionStart = editText.getSelectionStart();
         final int selectionEnd = editText.getSelectionEnd();
-        Pair<String, Integer> mentionProposal = CommentsUtil.getUserNameForMentionProposal(s.toString(), selectionStart);
-        mentionProposerLayout.removeAllViews();
+        final Pair<String, Integer> mentionProposal = CommentsUtil.getUserNameForMentionProposal(s.toString(), selectionStart);
         if (mentionProposal == null || (mentionProposal.first != null && mentionProposal.first.length() == 0) || selectionStart != selectionEnd) {
+            mentionProposerLayout.removeAllViews();
             mentionProposerLayout.setVisibility(View.GONE);
+            this.users.clear();
         } else {
             if (mentionProposal.first != null && mentionProposal.second != null) {
-                LiveDataHelper.observeOnce(syncManager.searchUserByUidOrDisplayName(account.getId(), boardLocalId, -1L, mentionProposal.first), owner, (users) -> {
-                    for (User user : users) {
-                        final ImageView avatar = new ImageView(mentionProposerLayout.getContext());
-                        avatar.setLayoutParams(layoutParams);
-                        mentionProposerLayout.addView(avatar);
-                        avatar.setOnClickListener((c) -> {
-                            editText.setText(s.subSequence(0, mentionProposal.second).toString() +
-                                    user.getUid() +
-                                    s.subSequence(mentionProposal.second + mentionProposal.first.length(), s.length()).toString());
-                            editText.setSelection(mentionProposal.second + user.getUid().length());
-                            mentionProposerLayout.setVisibility(View.GONE);
-                        });
+                observeOnce(syncManager.searchUserByUidOrDisplayName(account.getId(), boardLocalId, -1L, mentionProposal.first), owner, (users) -> {
+                    if (!users.equals(this.users)) {
+                        mentionProposerLayout.removeAllViews();
+                        for (User user : users) {
+                            final ImageView avatar = new ImageView(mentionProposerLayout.getContext());
+                            avatar.setLayoutParams(layoutParams);
+                            avatar.setOnClickListener((c) -> {
+                                editText.setText(
+                                        s.subSequence(0, mentionProposal.second) +
+                                                user.getUid() +
+                                                s.subSequence(mentionProposal.second + mentionProposal.first.length(), s.length())
+                                );
+                                editText.setSelection(mentionProposal.second + user.getUid().length());
+                                mentionProposerLayout.setVisibility(View.GONE);
+                            });
+                            mentionProposerLayout.addView(avatar);
 
-                        Glide.with(avatar.getContext())
-                                .load(account.getUrl() + "/index.php/avatar/" + Uri.encode(user.getUid()) + "/" + avatarSize)
-                                .error(R.drawable.ic_person_grey600_24dp)
-                                .apply(RequestOptions.circleCropTransform())
-                                .into(avatar);
+                            Glide.with(avatar.getContext())
+                                    .load(account.getUrl() + "/index.php/avatar/" + Uri.encode(user.getUid()) + "/" + avatarSize)
+                                    .error(R.drawable.ic_person_grey600_24dp)
+                                    .apply(RequestOptions.circleCropTransform())
+                                    .into(avatar);
+                        }
+                        this.users.clear();
+                        this.users.addAll(users);
                     }
                 });
+            } else {
+                this.users.clear();
+                mentionProposerLayout.removeAllViews();
             }
             mentionProposerLayout.setVisibility(View.VISIBLE);
         }
