db1fbc39869fe1f76a186ac50d8e24ae92bf3315
desperateCoder <echotodevnull@gmail.com>
2020-11-02 18:23:12 +0100

    #597 removed lombok for @stefan-niedermann

--- app/src/main/java/it/niedermann/nextcloud/deck/model/widget/filter/FilterWidget.java
+++ app/src/main/java/it/niedermann/nextcloud/deck/model/widget/filter/FilterWidget.java
@@ -1,15 +1,5 @@
 package it.niedermann.nextcloud.deck.model.widget.filter;
 
-import androidx.room.Entity;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Entity()
 public class FilterWidget {
 
     private Long id;
--- app/src/main/java/it/niedermann/nextcloud/deck/model/widget/filter/FilterWidgetSort.java
+++ app/src/main/java/it/niedermann/nextcloud/deck/model/widget/filter/FilterWidgetSort.java
@@ -1,12 +1,5 @@
 package it.niedermann.nextcloud.deck.model.widget.filter;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class FilterWidgetSort {
 
     private Long id;
