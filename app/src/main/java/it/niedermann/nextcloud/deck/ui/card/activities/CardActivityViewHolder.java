8f019592991b48b96bdb1269952abfea52184cf2
Stefan Niedermann <info@niedermann.it>
2020-11-18 12:57:07 +0100

    Fix icons in card activity
    Signed-off-by: Stefan Niedermann <info@niedermann.it>

--- app/src/main/java/it/niedermann/nextcloud/deck/ui/card/activities/CardActivityViewHolder.java
+++ app/src/main/java/it/niedermann/nextcloud/deck/ui/card/activities/CardActivityViewHolder.java
@@ -3,6 +3,7 @@ package it.niedermann.nextcloud.deck.ui.card.activities;
 import android.content.Context;
 import android.view.MenuInflater;
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -13,6 +14,7 @@ import it.niedermann.nextcloud.deck.databinding.ItemActivityBinding;
 import it.niedermann.nextcloud.deck.model.enums.ActivityType;
 import it.niedermann.nextcloud.deck.model.ocs.Activity;
 import it.niedermann.nextcloud.deck.util.DateUtil;
+import it.niedermann.nextcloud.deck.util.ViewUtil;
 
 public class CardActivityViewHolder extends RecyclerView.ViewHolder {
     public ItemActivityBinding binding;
@@ -32,33 +34,54 @@ public class CardActivityViewHolder extends RecyclerView.ViewHolder {
             inflater.inflate(R.menu.activity_menu, menu);
             menu.findItem(android.R.id.copy).setOnMenuItemClickListener(item -> ClipboardUtil.INSTANCE.copyToClipboard(context, activity.getSubject()));
         });
-        switch (ActivityType.findById(activity.getType())) {
+        final ActivityType type = ActivityType.findById(activity.getType());
+        setImageResource(binding.type, type);
+        setImageColor(context, binding.type, type);
+    }
+
+    private static void setImageResource(@NonNull ImageView imageView, @NonNull ActivityType type) {
+        switch (type) {
             case CHANGE:
-                binding.type.setImageResource(R.drawable.type_change_36dp);
+                imageView.setImageResource(R.drawable.type_change_36dp);
                 break;
             case ADD:
-                binding.type.setImageResource(R.drawable.type_add_color_36dp);
+                imageView.setImageResource(R.drawable.type_add_color_36dp);
                 break;
             case DELETE:
-                binding.type.setImageResource(R.drawable.type_delete_color_36dp);
+                imageView.setImageResource(R.drawable.type_delete_color_36dp);
                 break;
             case ARCHIVE:
-                binding.type.setImageResource(R.drawable.type_archive_grey600_36dp);
+                imageView.setImageResource(R.drawable.type_archive_grey600_36dp);
                 break;
             case TAGGED_WITH_LABEL:
-                binding.type.setImageResource(R.drawable.type_label_grey600_36dp);
+                imageView.setImageResource(R.drawable.type_label_grey600_36dp);
                 break;
             case COMMENT:
-                binding.type.setImageResource(R.drawable.type_comment_grey600_36dp);
+                imageView.setImageResource(R.drawable.type_comment_grey600_36dp);
                 break;
             case FILES:
-                binding.type.setImageResource(R.drawable.type_file_36dp);
+                imageView.setImageResource(R.drawable.type_file_36dp);
                 break;
             case HISTORY:
-                binding.type.setImageResource(R.drawable.type_history_36dp);
+                imageView.setImageResource(R.drawable.type_history_36dp);
                 break;
             case DECK:
             default:
+                imageView.setImageResource(R.drawable.ic_app_logo);
+                break;
+        }
+    }
+
+    private static void setImageColor(@NonNull Context context, @NonNull ImageView imageView, @NonNull ActivityType type) {
+        switch (type) {
+            case ADD:
+                ViewUtil.setImageColor(context, imageView, R.color.activity_create);
+                break;
+            case DELETE:
+                ViewUtil.setImageColor(context, imageView, R.color.activity_delete);
+                break;
+            default:
+                ViewUtil.setImageColor(context, imageView, R.color.grey600);
                 break;
         }
     }
