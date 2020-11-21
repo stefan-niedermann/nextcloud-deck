8337afa248e314732775833b05096835173af40b
Stefan Niedermann <info@niedermann.it>
2020-11-21 14:20:56 +0100

    Some work at the UI for the Filter Widget
    Signed-off-by: Stefan Niedermann <info@niedermann.it>

--- app/src/main/java/it/niedermann/nextcloud/deck/ui/widget/filter/FilterWidgetConfigurationActivity.java
+++ app/src/main/java/it/niedermann/nextcloud/deck/ui/widget/filter/FilterWidgetConfigurationActivity.java
@@ -11,8 +11,7 @@ import androidx.lifecycle.ViewModelProvider;
 import it.niedermann.nextcloud.deck.DeckLog;
 import it.niedermann.nextcloud.deck.R;
 import it.niedermann.nextcloud.deck.databinding.ActivityFilterWidgetBinding;
-import it.niedermann.nextcloud.deck.model.Account;
-import it.niedermann.nextcloud.deck.ui.widget.stack.StackWidget;
+import it.niedermann.nextcloud.deck.ui.exception.ExceptionHandler;
 
 public class FilterWidgetConfigurationActivity extends AppCompatActivity {
     private int appWidgetId;
@@ -23,8 +22,13 @@ public class FilterWidgetConfigurationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        Thread.setDefaultUncaughtExceptionHandler(new ExceptionHandler(this));
+
         binding = ActivityFilterWidgetBinding.inflate(getLayoutInflater());
         viewModel = new ViewModelProvider(this).get(FilterWidgetViewModel.class);
+
+        setContentView(binding.getRoot());
+        setSupportActionBar(binding.toolbar);
 
         final ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
@@ -32,35 +36,32 @@ public class FilterWidgetConfigurationActivity extends AppCompatActivity {
         }
 
         setResult(RESULT_CANCELED);
-        final Bundle extras = getIntent().getExtras();
+        final Bundle args = getIntent().getExtras();
 
-        if (extras != null) {
-            appWidgetId = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID,
-                    AppWidgetManager.INVALID_APPWIDGET_ID);
+        if (args != null) {
+            appWidgetId = args.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
         }
 
         if (appWidgetId == AppWidgetManager.INVALID_APPWIDGET_ID) {
             DeckLog.error("INVALID_APPWIDGET_ID");
             finish();
         }
-    }
+        binding.cancel.setOnClickListener((v) -> finish());
+        binding.submit.setOnClickListener((v) -> {
+            final Bundle extras = new Bundle();
 
-    protected void onSubmit(Account account, long boardId, long stackId) {
-        final Bundle extras = new Bundle();
+            viewModel.updateFilterWidget();
+            Intent updateIntent = new Intent(AppWidgetManager.ACTION_APPWIDGET_UPDATE, null, getApplicationContext(), FilterWidget.class);
+            extras.putInt(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
 
-        viewModel.updateFilterWidget();
-        Intent updateIntent = new Intent(AppWidgetManager.ACTION_APPWIDGET_UPDATE, null,
-                getApplicationContext(), StackWidget.class);
-        extras.putSerializable(StackWidget.ACCOUNT_KEY, account);
-        extras.putInt(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+            // The `extras` bundle is added to the intent this way because using putExtras(extras)
+            // would have the OS attempt to reassemle the data and cause a crash
+            // when it finds classes that are only known to this application.
+            updateIntent.putExtra(FilterWidget.BUNDLE_KEY, extras);
+            setResult(RESULT_OK, updateIntent);
+            getApplicationContext().sendBroadcast(updateIntent);
 
-        // The `extras` bundle is added to the intent this way because using putExtras(extras) 
-        // would have the OS attempt to reassemle the data and cause a crash 
-        // when it finds classes that are only known to this application.
-        updateIntent.putExtra(StackWidget.BUNDLE_KEY, extras);
-        setResult(RESULT_OK, updateIntent);
-        getApplicationContext().sendBroadcast(updateIntent);
-
-        finish();
+            finish();
+        });
     }
 }
