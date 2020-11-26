56d03fe68be4fb6e7fa1743d66d0a01d0dc30a9f
Stefan Niedermann <info@niedermann.it>
2020-11-26 11:48:29 +0100

    MarkwonMarkdownUtil: Support syntax highlighting
    Signed-off-by: Stefan Niedermann <info@niedermann.it>

--- markdown/src/main/java/it/niedermann/android/markdown/markwon/MarkwonMarkdownUtil.java
+++ markdown/src/main/java/it/niedermann/android/markdown/markwon/MarkwonMarkdownUtil.java
@@ -15,18 +15,30 @@ import io.noties.markwon.image.glide.GlideImagesPlugin;
 import io.noties.markwon.inlineparser.MarkwonInlineParserPlugin;
 import io.noties.markwon.linkify.LinkifyPlugin;
 import io.noties.markwon.simple.ext.SimpleExtPlugin;
+import io.noties.markwon.syntax.Prism4jTheme;
+import io.noties.markwon.syntax.Prism4jThemeDefault;
+import io.noties.markwon.syntax.SyntaxHighlightPlugin;
+import io.noties.prism4j.Prism4j;
+import io.noties.prism4j.annotations.PrismBundle;
 import it.niedermann.android.markdown.markwon.plugins.NextcloudMentionsPlugin;
 
-/**
- * Created by stefan on 07.12.16.
- */
 @RestrictTo(value = RestrictTo.Scope.LIBRARY)
+@PrismBundle(
+        include = {
+                "c", "clike", "clojure", "cpp", "csharp", "css", "dart", "git", "go", "groovy", "java", "javascript", "json",
+                "kotlin", "latex", "makefile", "markdown", "markup", "python", "scala", "sql", "swift", "yaml"
+        },
+        grammarLocatorClassName = ".MarkwonGrammarLocator"
+)
 public class MarkwonMarkdownUtil {
 
     private MarkwonMarkdownUtil() {
+        // Util class
     }
 
     public static Markwon.Builder initMarkwon(@NonNull Context context) {
+        final Prism4j prism4j = new Prism4j(new MarkwonGrammarLocator());
+        final Prism4jTheme prism4jTheme = Prism4jThemeDefault.create();
         return Markwon.builder(context)
                 .usePlugin(StrikethroughPlugin.create())
                 .usePlugin(TaskListPlugin.create(context))
@@ -34,7 +46,8 @@ public class MarkwonMarkdownUtil {
                 .usePlugin(GlideImagesPlugin.create(context))
                 .usePlugin(SimpleExtPlugin.create())
                 .usePlugin(MarkwonInlineParserPlugin.create())
-                .usePlugin(LinkifyPlugin.create());
+                .usePlugin(LinkifyPlugin.create())
+                .usePlugin(SyntaxHighlightPlugin.create(prism4j, prism4jTheme));
     }
 
     public static Markwon.Builder initMarkwon(@NonNull Context context, @NonNull Map<String, String> mentions) {
