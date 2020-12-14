d85422afe0e529ef642712e830bd67be30ee5d46
Stefan Niedermann <info@niedermann.it>
2020-12-14 13:52:03 +0100

    Separators by DueType in UpcomingWidget
    Signed-off-by: Stefan Niedermann <info@niedermann.it>

--- app/src/main/java/it/niedermann/nextcloud/deck/ui/widget/upcoming/UpcomingWidgetFactory.java
+++ app/src/main/java/it/niedermann/nextcloud/deck/ui/widget/upcoming/UpcomingWidgetFactory.java
@@ -8,23 +8,30 @@ import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import it.niedermann.nextcloud.deck.DeckLog;
 import it.niedermann.nextcloud.deck.R;
 import it.niedermann.nextcloud.deck.api.IResponseCallback;
+import it.niedermann.nextcloud.deck.model.enums.EDueType;
 import it.niedermann.nextcloud.deck.model.full.FullCard;
 import it.niedermann.nextcloud.deck.model.widget.filter.dto.FilterWidgetCard;
 import it.niedermann.nextcloud.deck.persistence.sync.SyncManager;
+
+import static java.time.temporal.ChronoUnit.DAYS;
 
 public class UpcomingWidgetFactory implements RemoteViewsService.RemoteViewsFactory {
     private final Context context;
     private final int appWidgetId;
 
     @NonNull
-    private final List<FilterWidgetCard> data = new ArrayList<>();
+    private final List<Object> data = new ArrayList<>();
 
     UpcomingWidgetFactory(Context context, Intent intent) {
         this.context = context;
@@ -39,7 +46,26 @@ public class UpcomingWidgetFactory implements RemoteViewsService.RemoteViewsFact
             @Override
             public void onResponse(List<FilterWidgetCard> response) {
                 data.clear();
-                data.addAll(response);
+                Collections.sort(response, (card1, card2) -> {
+                    if (card1 == null || card1.getCard() == null || card1.getCard().getCard().getDueDate() == null) {
+                        return -1;
+                    }
+                    if (card2 == null || card2.getCard() == null || card2.getCard().getCard().getDueDate() == null) {
+                        return 1;
+                    }
+                    return card1.getCard().getCard().getDueDate().compareTo(card2.getCard().getCard().getDueDate()) * -1;
+                });
+                EDueType lastDueType = null;
+                for (FilterWidgetCard filterWidgetCard : response) {
+                    if (filterWidgetCard.getCard().getCard().getDueDate() != null) {
+                        final EDueType nextDueType = getDueType(context, filterWidgetCard.getCard().getCard().getDueDate().atZone(ZoneId.systemDefault()).toLocalDate());
+                        if (!nextDueType.equals(lastDueType)) {
+                            data.add(new Separator(EDueType.OVERDUE.toString(context)));
+                            lastDueType = nextDueType;
+                        }
+                    }
+                    data.add(filterWidgetCard);
+                }
                 final RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_upcoming);
                 notifyAppWidgetUpdate(views);
             }
@@ -51,7 +77,6 @@ public class UpcomingWidgetFactory implements RemoteViewsService.RemoteViewsFact
 
     }
 
-
     @Override
     public void onDestroy() {
 
@@ -59,22 +84,22 @@ public class UpcomingWidgetFactory implements RemoteViewsService.RemoteViewsFact
 
     @Override
     public int getCount() {
-        return data.size() + 1;
+        return data.size();
     }
 
     @Override
     public RemoteViews getViewAt(int i) {
-        RemoteViews widget_entry;
-        if (i == 0) {
+        if (i > (data.size() - 1) || data.get(i) == null) {
+            DeckLog.error("No card or separator not found at position " + i);
+            return null;
+        }
+        final RemoteViews widget_entry;
+        if (data.get(i).getClass() == Separator.class) {
+            final Separator separator = (Separator) data.get(i);
             widget_entry = new RemoteViews(context.getPackageName(), R.layout.widget_separator);
-            widget_entry.setTextViewText(R.id.widget_entry_content_tv, "First headline");
+            widget_entry.setTextViewText(R.id.widget_entry_content_tv, separator.title);
         } else {
-            int cardPosition = i - 1;
-            if (cardPosition > (data.size() - 1) || data.get(cardPosition) == null) {
-                DeckLog.error("Card not found at position " + cardPosition);
-                return null;
-            }
-            final FullCard card = data.get(cardPosition).getCard();
+            final FullCard card = ((FilterWidgetCard) data.get(i)).getCard();
             widget_entry = new RemoteViews(context.getPackageName(), R.layout.widget_stack_entry);
             widget_entry.setTextViewText(R.id.widget_entry_content_tv, card.getCard().getTitle());
         }
@@ -106,5 +131,32 @@ public class UpcomingWidgetFactory implements RemoteViewsService.RemoteViewsFact
         int[] appWidgetIds = awm.getAppWidgetIds(new ComponentName(context, UpcomingWidget.class));
         awm.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.upcoming_widget_lv);
         awm.updateAppWidget(appWidgetId, views);
+    }
+
+    private static EDueType getDueType(@NonNull Context context, @Nullable LocalDate dueDate) {
+        if (dueDate == null) {
+            return EDueType.NO_DUE;
+        }
+
+        long diff = DAYS.between(LocalDate.now(), dueDate);
+
+        if (diff > 7 && diff <= 30) {
+            return EDueType.MONTH;
+        } else if (diff > 0 && diff <= 7) {
+            return EDueType.WEEK;
+        } else if (diff == 0) {
+            return EDueType.TODAY;
+        } else if (diff < 0) {
+            return EDueType.OVERDUE;
+        }
+        return EDueType.NO_FILTER;
+    }
+
+    private static class Separator {
+        public String title;
+
+        private Separator(String title) {
+            this.title = title;
+        }
     }
 }
