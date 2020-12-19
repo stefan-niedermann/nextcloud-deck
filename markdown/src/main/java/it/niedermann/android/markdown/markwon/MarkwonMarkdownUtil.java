195b85204ceb6eaacc322fd8d6fbe055b363a749
Stefan Niedermann <info@niedermann.it>
2020-12-19 17:30:19 +0100

    Comment search part out
    Signed-off-by: Stefan Niedermann <info@niedermann.it>

--- markdown/src/main/java/it/niedermann/android/markdown/markwon/MarkwonMarkdownUtil.java
+++ markdown/src/main/java/it/niedermann/android/markdown/markwon/MarkwonMarkdownUtil.java
@@ -30,7 +30,6 @@ import io.noties.prism4j.Prism4j;
 import io.noties.prism4j.annotations.PrismBundle;
 import it.niedermann.android.markdown.markwon.model.EListType;
 import it.niedermann.android.markdown.markwon.plugins.NextcloudMentionsPlugin;
-import it.niedermann.android.markdown.markwon.plugins.SearchHighlightPlugin;
 import it.niedermann.android.markdown.markwon.plugins.ThemePlugin;
 import it.niedermann.android.markdown.markwon.span.SearchSpan;
 
@@ -58,8 +57,8 @@ public class MarkwonMarkdownUtil {
                 .usePlugin(StrikethroughPlugin.create())
                 .usePlugin(SimpleExtPlugin.create())
                 .usePlugin(ImagesPlugin.create())
-                .usePlugin(MarkwonInlineParserPlugin.create())
-                .usePlugin(SearchHighlightPlugin.create(context));
+                .usePlugin(MarkwonInlineParserPlugin.create());
+//                .usePlugin(SearchHighlightPlugin.create(context));
     }
 
     public static Markwon.Builder initMarkwonViewer(@NonNull Context context) {
