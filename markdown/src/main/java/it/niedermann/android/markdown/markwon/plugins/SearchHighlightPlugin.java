c55dc717e4fa6c24f361e42a7136c72a52ccc929
Stefan Niedermann <info@niedermann.it>
2020-12-20 13:43:38 +0100

    Enhance rendering performance of TextView when highlighting search results
    Signed-off-by: Stefan Niedermann <info@niedermann.it>

--- markdown/src/main/java/it/niedermann/android/markdown/markwon/plugins/SearchHighlightPlugin.java
+++ markdown/src/main/java/it/niedermann/android/markdown/markwon/plugins/SearchHighlightPlugin.java
@@ -1,8 +1,10 @@
 package it.niedermann.android.markdown.markwon.plugins;
 
 import android.content.Context;
-import android.text.Editable;
+import android.text.Spannable;
+import android.text.SpannableString;
 import android.text.SpannableStringBuilder;
+import android.util.Log;
 import android.widget.TextView;
 
 import androidx.annotation.ColorInt;
@@ -16,6 +18,8 @@ import it.niedermann.android.markdown.R;
 import it.niedermann.android.markdown.markwon.MarkwonMarkdownUtil;
 
 public class SearchHighlightPlugin extends AbstractMarkwonPlugin {
+
+    private static final String TAG = SearchHighlightPlugin.class.getSimpleName();
 
     private boolean searchActive = false;
     private CharSequence searchText;
@@ -47,9 +51,15 @@ public class SearchHighlightPlugin extends AbstractMarkwonPlugin {
     public void afterSetText(@NonNull TextView textView) {
         super.afterSetText(textView);
         if (this.searchActive) {
-            final Editable coloredContent = new SpannableStringBuilder(textView.getText());
-            MarkwonMarkdownUtil.searchAndColor(coloredContent, searchText, textView.getContext(), current, color);
-            textView.setText(coloredContent, TextView.BufferType.SPANNABLE);
+            final CharSequence content = textView.getText();
+            if (content.getClass() == SpannableString.class || content instanceof Spannable) {
+                MarkwonMarkdownUtil.searchAndColor((Spannable) content, searchText, textView.getContext(), current, color);
+            } else {
+                Log.w(TAG, "Expected " + TextView.class.getSimpleName() + " content to be of type " + Spannable.class.getSimpleName() + ", but was of type " + content.getClass() + ". Search highlighting will be not performant.");
+                final Spannable coloredContent = new SpannableStringBuilder(content);
+                textView.setText(coloredContent, TextView.BufferType.SPANNABLE);
+                MarkwonMarkdownUtil.searchAndColor(coloredContent, searchText, textView.getContext(), current, color);
+            }
         }
     }
 }
