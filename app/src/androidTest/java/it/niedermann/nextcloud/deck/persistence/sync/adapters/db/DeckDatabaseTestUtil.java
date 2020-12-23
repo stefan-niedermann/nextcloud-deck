fc62940991439af71a7fd932cadd1a5cac793318
Stefan Niedermann <info@niedermann.it>
2020-12-23 18:21:25 +0100

    Use random values in db tests
    Signed-off-by: Stefan Niedermann <info@niedermann.it>

--- app/src/androidTest/java/it/niedermann/nextcloud/deck/persistence/sync/adapters/db/DeckDatabaseTestUtil.java
+++ app/src/androidTest/java/it/niedermann/nextcloud/deck/persistence/sync/adapters/db/DeckDatabaseTestUtil.java
@@ -2,6 +2,8 @@ package it.niedermann.nextcloud.deck.persistence.sync.adapters.db;
 
 import androidx.annotation.NonNull;
 
+import java.util.Random;
+
 import it.niedermann.nextcloud.deck.model.Account;
 import it.niedermann.nextcloud.deck.model.User;
 import it.niedermann.nextcloud.deck.persistence.sync.adapters.db.dao.AccountDao;
@@ -15,19 +17,30 @@ public class DeckDatabaseTestUtil {
 
     public static Account createAccount(@NonNull AccountDao dao) {
         final Account accountToCreate = new Account();
-        accountToCreate.setName("test@example.com");
-        accountToCreate.setUserName("test");
-        accountToCreate.setUrl("https://example.com");
-        dao.insert(accountToCreate);
-        return dao.getAccountByNameDirectly("test@example.com");
+        accountToCreate.setName(randomString(15) + " " + randomString(15));
+        accountToCreate.setUserName(randomString(10));
+        accountToCreate.setUrl("https://" + randomString(10) + ".example.com");
+        final long id = dao.insert(accountToCreate);
+        return dao.getAccountByIdDirectly(id);
     }
 
     public static User createUser(@NonNull UserDao dao, @NonNull Account account) {
         final User userToCreate = new User();
-        userToCreate.setDisplayname("Test Tester");
-        userToCreate.setUid("test");
+        userToCreate.setDisplayname(randomString(15) + " " + randomString(15));
+        userToCreate.setUid(randomString(10));
         userToCreate.setAccountId(account.getId());
-        dao.insert(userToCreate);
-        return dao.getUserByUidDirectly(account.getId(), "test");
+        final long id = dao.insert(userToCreate);
+        return dao.getUserByLocalIdDirectly(id);
+    }
+
+    private static String randomString(int length) {
+        final int leftLimit = 48; // numeral '0'
+        final int rightLimit = 122; // letter 'z'
+
+        return new Random().ints(leftLimit, rightLimit + 1)
+                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
+                .limit(length)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
     }
 }
