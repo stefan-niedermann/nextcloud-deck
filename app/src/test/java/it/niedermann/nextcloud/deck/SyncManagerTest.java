99226e21adc488bdcbb8ca1179afe172ced22dd6
Stefan Niedermann <info@niedermann.it>
2020-12-27 22:38:59 +0100

    Add testSynchronizeBoard
    Signed-off-by: Stefan Niedermann <info@niedermann.it>

--- app/src/test/java/it/niedermann/nextcloud/deck/SyncManagerTest.java
+++ app/src/test/java/it/niedermann/nextcloud/deck/SyncManagerTest.java
@@ -16,20 +16,33 @@ import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import it.niedermann.nextcloud.deck.api.IResponseCallback;
+import it.niedermann.nextcloud.deck.exceptions.OfflineException;
 import it.niedermann.nextcloud.deck.model.Account;
+import it.niedermann.nextcloud.deck.model.full.FullBoard;
 import it.niedermann.nextcloud.deck.persistence.sync.SyncManager;
 import it.niedermann.nextcloud.deck.persistence.sync.adapters.ServerAdapter;
 import it.niedermann.nextcloud.deck.persistence.sync.adapters.db.DataBaseAdapter;
+import it.niedermann.nextcloud.deck.persistence.sync.helpers.SyncHelper;
+import it.niedermann.nextcloud.deck.persistence.sync.helpers.providers.AbstractSyncDataProvider;
+import it.niedermann.nextcloud.deck.persistence.sync.helpers.providers.StackDataProvider;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.powermock.api.mockito.PowerMockito.whenNew;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({SyncManager.class})
+@PrepareForTest({SyncManager.class, DeckLog.class})
 public class SyncManagerTest {
 
     @Rule
@@ -41,18 +54,55 @@ public class SyncManagerTest {
     private ServerAdapter serverAdapter;
     @Mock
     private DataBaseAdapter dataBaseAdapter;
+    @Mock
+    private DeckLog deckLog;
 
     @InjectMocks
     private SyncManager syncManager;
 
     @Before
     public void prepareDoAsync() {
+        PowerMockito.mockStatic(DeckLog.class);
         PowerMockito
                 .replace(PowerMockito.method(SyncManager.class, "doAsync", Runnable.class))
                 .with((obj, method, arguments) -> {
                     ((Runnable) arguments[0]).run();
                     return null;
                 });
+    }
+
+    /**
+     * When {@link SyncManager#synchronizeBoard(IResponseCallback, long)} is triggered, it should
+     * pass the given {@link IResponseCallback} to the {@link SyncHelper} and trigger a
+     * {@link SyncHelper#doSyncFor(AbstractSyncDataProvider)}.
+     * {@link OfflineException} should be caught and passed to the {@link IResponseCallback}
+     */
+    @Test
+    public void testSynchronizeBoard() throws Exception {
+        final SyncHelper syncHelper = mock(SyncHelper.class);
+
+        when(dataBaseAdapter.getFullBoardByLocalIdDirectly(anyLong(), anyLong())).thenReturn(new FullBoard());
+        when(syncHelper.setResponseCallback(any())).thenReturn(syncHelper);
+        doNothing().when(syncHelper).doSyncFor(any());
+        whenNew(SyncHelper.class).withAnyArguments().thenReturn(syncHelper);
+
+        IResponseCallback<Boolean> responseCallback = spy(new IResponseCallback<Boolean>(new Account(1L)) {
+            @Override
+            public void onResponse(Boolean response) {
+
+            }
+        });
+
+        syncManager.synchronizeBoard(responseCallback, 1L);
+
+        verify(syncHelper, times(1)).setResponseCallback(responseCallback);
+        verify(syncHelper, times(1)).doSyncFor(any(StackDataProvider.class));
+
+        doThrow(OfflineException.class).when(syncHelper).doSyncFor(any());
+
+        syncManager.synchronizeBoard(responseCallback, 1L);
+
+        verify(responseCallback, times(1)).onError(any(OfflineException.class));
     }
 
     @Test
