5343248f58dc4c658efb82e032d77e3e457dc66b
desperateCoder <echotodevnull@gmail.com>
2021-01-31 13:44:59 +0100

    #800 add cards of non-shared boards to result list

--- app/src/main/java/it/niedermann/nextcloud/deck/persistence/sync/adapters/db/dao/CardDao.java
+++ app/src/main/java/it/niedermann/nextcloud/deck/persistence/sync/adapters/db/dao/CardDao.java
@@ -85,4 +85,11 @@ public interface CardDao extends GenericDao<Card> {
 
     @Query("SELECT c.stackId FROM card c WHERE  localId = :localCardId")
     Long getLocalStackIdByLocalCardId(Long localCardId);
+
+    @Transaction
+    @Query("SELECT * FROM card c WHERE " +
+            "exists(select 1 from Stack s join Board b on s.boardId = b.localId where s.localId = c.stackId and not exists(select 1 from AccessControl ac where ac.boardId = b.localId)) " +
+            "and ((:accountIds) is null or accountId in (:accountIds)) " +
+            "and (status<>1 or id is null or lastModified <> lastModifiedLocal)")
+    List<FullCard> getFullCardsForNonSharedBoardsDirectly(List<Long> accountIds);
 }
