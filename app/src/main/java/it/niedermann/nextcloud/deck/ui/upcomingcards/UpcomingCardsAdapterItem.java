53cd7fed7e1089eef5f3fa8f8bfb5f9d1f0097a6
Stefan Niedermann <info@niedermann.it>
2021-04-09 17:52:44 +0200

    #690 Upcoming cards
    Add @NonNull / @Nullable annotations
    
    Signed-off-by: Stefan Niedermann <info@niedermann.it>

--- app/src/main/java/it/niedermann/nextcloud/deck/ui/upcomingcards/UpcomingCardsAdapterItem.java
+++ app/src/main/java/it/niedermann/nextcloud/deck/ui/upcomingcards/UpcomingCardsAdapterItem.java
@@ -1,16 +1,24 @@
 package it.niedermann.nextcloud.deck.ui.upcomingcards;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import org.jetbrains.annotations.NotNull;
+
 import it.niedermann.nextcloud.deck.model.Account;
 import it.niedermann.nextcloud.deck.model.full.FullCard;
 
 public class UpcomingCardsAdapterItem {
+    @NonNull
     private final FullCard fullCard;
+    @NonNull
     private final Account account;
     private final long currentBoardLocalId;
+    @Nullable
     private final Long currentBoardRemoteId;
     private final boolean currentBoardHasEditPermission;
 
-    public UpcomingCardsAdapterItem(FullCard fullCard, Account account, long currentBoardLocalId, Long currentBoardRemoteId, boolean currentBoardHasEditPermission) {
+    public UpcomingCardsAdapterItem(@NonNull FullCard fullCard, @NonNull Account account, long currentBoardLocalId, @Nullable Long currentBoardRemoteId, boolean currentBoardHasEditPermission) {
         this.fullCard = fullCard;
         this.account = account;
         this.currentBoardLocalId = currentBoardLocalId;
@@ -18,11 +26,11 @@ public class UpcomingCardsAdapterItem {
         this.currentBoardHasEditPermission = currentBoardHasEditPermission;
     }
 
-    public FullCard getFullCard() {
+    public @NotNull FullCard getFullCard() {
         return fullCard;
     }
 
-    public Account getAccount() {
+    public @NotNull Account getAccount() {
         return account;
     }
 
@@ -30,7 +38,7 @@ public class UpcomingCardsAdapterItem {
         return currentBoardLocalId;
     }
 
-    public Long getCurrentBoardRemoteId() {
+    public @org.jetbrains.annotations.Nullable Long getCurrentBoardRemoteId() {
         return currentBoardRemoteId;
     }
 
