961a7a5e212dbd1b58516eb89a4f95c0292a8045
Stefan Niedermann <info@niedermann.it>
2021-04-09 20:33:01 +0200

    #690 Upcoming cards
    Use new method for UpcomingCardsWidget
    
    Signed-off-by: Stefan Niedermann <info@niedermann.it>

--- app/src/main/java/it/niedermann/nextcloud/deck/ui/upcomingcards/UpcomingCardsUtil.java
+++ app/src/main/java/it/niedermann/nextcloud/deck/ui/upcomingcards/UpcomingCardsUtil.java
@@ -1,11 +1,21 @@
 package it.niedermann.nextcloud.deck.ui.upcomingcards;
 
+import android.content.Context;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import it.niedermann.nextcloud.deck.DeckLog;
+import it.niedermann.nextcloud.deck.model.Card;
 
 import static java.time.temporal.ChronoUnit.DAYS;
 
@@ -34,5 +44,59 @@ public class UpcomingCardsUtil {
         } else {
             return EUpcomingDueType.OVERDUE;
         }
+    }
+
+    /**
+     * @return a {@link Collection} of the given {@param items}, separated by {@param UpcomingCardsAdapterSectionItem}.
+     */
+    public static Collection<Object> addDueDateSeparators(@NonNull Context context, @NonNull List<UpcomingCardsAdapterItem> items) {
+        final Collection<Object> ret = new ArrayList<>();
+
+        final Comparator<UpcomingCardsAdapterItem> comparator = Comparator.comparing((card -> {
+            if (card != null &&
+                    card.getFullCard() != null &&
+                    card.getFullCard().getCard() != null &&
+                    card.getFullCard().getCard().getDueDate() != null) {
+                return card.getFullCard().getCard().getDueDate();
+            }
+            return null;
+        }), Comparator.nullsLast(Comparator.naturalOrder()));
+
+        comparator.thenComparing(card -> {
+            if (card != null &&
+                    card.getFullCard() != null &&
+                    card.getFullCard().getCard().getDueDate() != null) {
+
+                final Card c = card.getFullCard().getCard();
+
+                if (c.getLastModified() == null && c.getLastModifiedLocal() != null) {
+                    return c.getLastModifiedLocal();
+                } else if (c.getLastModified() != null && c.getLastModifiedLocal() == null) {
+                    return c.getLastModified();
+                } else {
+                    return c.getLastModifiedLocal().toEpochMilli() > c.getLastModified().toEpochMilli() ?
+                            c.getLastModifiedLocal() : c.getLastModified();
+                }
+            }
+            return null;
+        }, Comparator.nullsLast(Comparator.naturalOrder()));
+
+        Collections.sort(
+                items,
+                comparator
+        );
+
+        EUpcomingDueType lastDueType = null;
+        for (UpcomingCardsAdapterItem filterWidgetCard : items) {
+            final EUpcomingDueType nextDueType = getDueType(filterWidgetCard.getFullCard().getCard().getDueDate());
+            DeckLog.info(filterWidgetCard.getFullCard().getCard().getTitle() + ":", nextDueType.name());
+            if (!nextDueType.equals(lastDueType)) {
+                ret.add(new UpcomingCardsAdapterSectionItem(nextDueType.toString(context)));
+                lastDueType = nextDueType;
+            }
+            ret.add(filterWidgetCard);
+        }
+
+        return ret;
     }
 }
