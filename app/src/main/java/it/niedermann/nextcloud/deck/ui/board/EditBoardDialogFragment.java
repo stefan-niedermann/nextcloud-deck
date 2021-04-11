f2b3484bc413a13f22216b3b521f4da099c10af3
Stefan Niedermann <info@niedermann.it>
2021-04-11 10:04:06 +0200

    Fix EditBoardDialogFragment branding
    Signed-off-by: Stefan Niedermann <info@niedermann.it>

--- app/src/main/java/it/niedermann/nextcloud/deck/ui/board/EditBoardDialogFragment.java
+++ app/src/main/java/it/niedermann/nextcloud/deck/ui/board/EditBoardDialogFragment.java
@@ -15,11 +15,10 @@ import it.niedermann.nextcloud.deck.R;
 import it.niedermann.nextcloud.deck.databinding.DialogTextColorInputBinding;
 import it.niedermann.nextcloud.deck.model.full.FullBoard;
 import it.niedermann.nextcloud.deck.ui.MainViewModel;
-import it.niedermann.nextcloud.deck.ui.branding.BrandedDialogFragment;
 
 import static it.niedermann.nextcloud.deck.ui.branding.BrandingUtil.applyBrandToEditTextInputLayout;
 
-public class EditBoardDialogFragment extends BrandedDialogFragment {
+public class EditBoardDialogFragment extends DialogFragment {
 
     private DialogTextColorInputBinding binding;
 
@@ -63,6 +62,7 @@ public class EditBoardDialogFragment extends BrandedDialogFragment {
                     String title = this.fullBoard.getBoard().getTitle();
                     binding.input.setText(title);
                     binding.input.setSelection(title.length());
+                    applyBrand(fb.getBoard().getColor());
                     binding.colorChooser.selectColor(fullBoard.getBoard().getColor());
                 }
             });
@@ -70,6 +70,7 @@ public class EditBoardDialogFragment extends BrandedDialogFragment {
             dialogBuilder.setTitle(R.string.add_board);
             dialogBuilder.setPositiveButton(R.string.simple_add, (dialog, which) -> editBoardListener.onCreateBoard(binding.input.getText().toString(), binding.colorChooser.getSelectedColor()));
             binding.colorChooser.selectColor(ContextCompat.getColor(requireContext(), R.color.board_default_color));
+            applyBrand(ContextCompat.getColor(requireContext(), R.color.accent));
         }
 
         return dialogBuilder
@@ -94,7 +95,6 @@ public class EditBoardDialogFragment extends BrandedDialogFragment {
         return newInstance(null);
     }
 
-    @Override
     public void applyBrand(int mainColor) {
         applyBrandToEditTextInputLayout(mainColor, binding.inputWrapper);
     }
