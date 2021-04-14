5f0436a1bf85ed1e2d668e28e6ce2b15a2a651f5
desperateCoder <echotodevnull@gmail.com>
2021-04-14 18:59:30 +0200

    #935 wtf did i just witness?!

--- app/src/main/java/it/niedermann/nextcloud/deck/persistence/sync/adapters/db/migration/Migration_30_31.java
+++ app/src/main/java/it/niedermann/nextcloud/deck/persistence/sync/adapters/db/migration/Migration_30_31.java
@@ -28,6 +28,7 @@ public class Migration_30_31 extends Migration {
         database.execSQL("CREATE UNIQUE INDEX `index_AccessControl_accountId_id` ON `AccessControl` (`accountId`, `id`)");
         database.execSQL("CREATE TABLE `Activity_tmp` (`localId` INTEGER PRIMARY KEY AUTOINCREMENT, `accountId` INTEGER NOT NULL, `id` INTEGER, `status` INTEGER NOT NULL, `lastModified` INTEGER, `lastModifiedLocal` INTEGER, `etag` TEXT, `cardId` INTEGER NOT NULL, `subject` TEXT, `type` INTEGER NOT NULL, FOREIGN KEY(`cardId`) REFERENCES `Card`(`localId`) ON UPDATE NO ACTION ON DELETE CASCADE , FOREIGN KEY(`accountId`) REFERENCES `Account`(`id`) ON DELETE CASCADE )");
         database.execSQL("DELETE FROM `Activity` where accountId not in (select id from `Account`)");
+        database.execSQL("UPDATE `Activity` SET `type` = 2 WHERE `type` IS NULL");
         database.execSQL("INSERT INTO `Activity_tmp` select * from `Activity`");
         database.execSQL("DROP TABLE `Activity`");
         database.execSQL("ALTER TABLE `Activity_tmp` RENAME TO `Activity`");
