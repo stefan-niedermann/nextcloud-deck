0a9059779ab58120e6dece7fe7aa69b2a123645a
Stefan Niedermann <info@niedermann.it>
2021-05-25 12:25:09 +0200

    Clean up build.gradle files
    Signed-off-by: Stefan Niedermann <info@niedermann.it>

--- cross-tab-drag-and-drop/src/main/java/it/niedermann/android/crosstabdnd/ScrollHelper.java
+++ cross-tab-drag-and-drop/src/main/java/it/niedermann/android/crosstabdnd/ScrollHelper.java
@@ -7,7 +7,7 @@ import androidx.recyclerview.widget.RecyclerView;
 @SuppressWarnings("WeakerAccess")
 public class ScrollHelper implements Runnable {
 
-    private static final int SROLL_SPEED = 200;
+    private static final int SCROLL_SPEED = 200;
 
     public enum ScrollDirection {
         UP,
@@ -17,7 +17,7 @@ public class ScrollHelper implements Runnable {
     private boolean shouldScroll = false;
     private ScrollDirection scrollDirection;
     private RecyclerView currentRecyclerView;
-    private Handler handler = new Handler();
+    private final Handler handler = new Handler();
 
     public void startScroll(RecyclerView recyclerView, ScrollDirection scrollDirection) {
         this.scrollDirection = scrollDirection;
@@ -35,9 +35,9 @@ public class ScrollHelper implements Runnable {
     @Override
     public void run() {
         if (scrollDirection == ScrollDirection.UP) {
-            currentRecyclerView.smoothScrollBy(0, SROLL_SPEED * -1);
+            currentRecyclerView.smoothScrollBy(0, SCROLL_SPEED * -1);
         } else {
-            currentRecyclerView.smoothScrollBy(0, SROLL_SPEED);
+            currentRecyclerView.smoothScrollBy(0, SCROLL_SPEED);
         }
         if (shouldScroll) {
             handler.postDelayed(this, 100);
