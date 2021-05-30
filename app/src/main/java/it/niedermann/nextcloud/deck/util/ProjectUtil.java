a31f3eb3f6ba7198331f9c39339a1fdff34c0973
Stefan Niedermann <info@niedermann.it>
2021-05-30 21:53:44 +0200

    Fix expected and actual results in ProjectUtilTest
    Signed-off-by: Stefan Niedermann <info@niedermann.it>

--- app/src/main/java/it/niedermann/nextcloud/deck/util/ProjectUtil.java
+++ app/src/main/java/it/niedermann/nextcloud/deck/util/ProjectUtil.java
@@ -51,12 +51,12 @@ public class ProjectUtil {
             throw new IllegalArgumentException("trimmed url is empty");
         }
         // extract important part
-        String[] splitByPrefix = url.split(".*(index\\.php/)?apps/deck(/#)?/board/");
+        final String[] splitByPrefix = url.split(".*(index\\.php/)?apps/deck(/#)?/board/");
         // split into board- and card part
         if (splitByPrefix.length < 2) {
             throw new IllegalArgumentException("This URL doesn't seem to be an URL containing the boardId: \"" + url + "\"");
         }
-        String[] splitBySeparator = splitByPrefix[1].split("/card/");
+        final String[] splitBySeparator = splitByPrefix[1].split("/card/");
 
         // remove any unexpected stuff
         if (splitBySeparator.length > 1 && splitBySeparator[1].contains("/")) {
@@ -71,14 +71,14 @@ public class ProjectUtil {
         }
 
         // return result
-        long boardId = Long.parseLong(splitBySeparator[0]);
+        final long boardId = Long.parseLong(splitBySeparator[0]);
         if (boardId < 1) {
             throw new IllegalArgumentException("Invalid boardId \"" + boardId + "\" for url \"" + url + "\".");
         }
         if (splitBySeparator.length == 1) {
             return new long[]{boardId};
         } else if (splitBySeparator.length == 2) {
-            long cardId = Long.parseLong(splitBySeparator[1]);
+            final long cardId = Long.parseLong(splitBySeparator[1]);
             if (cardId > 0) {
                 return new long[]{boardId, cardId};
             } else {
--- app/src/test/java/it/niedermann/nextcloud/deck/ProjectUtilTest.java
+++ app/src/test/java/it/niedermann/nextcloud/deck/ProjectUtilTest.java
@@ -11,88 +11,88 @@ public class ProjectUtilTest {
     @Test
     public void extractBoardIdAndCardIdFromUrl() {
         // Valid board URLs with # and with index.php
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("index.php/apps/deck/#/board/4"), new long[]{4});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("/index.php/apps/deck/#/board/4"), new long[]{4});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("example.com/index.php/apps/deck/#/board/4"), new long[]{4});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("http://example.com/index.php/apps/deck/#/board/4"), new long[]{4});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/index.php/apps/deck/#/board/4"), new long[]{4});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/index.php/apps/deck/#/board/4"), new long[]{4});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/index.php/apps/deck/#/board/4/card"), new long[]{4});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/index.php/apps/deck/#/board/4/card/"), new long[]{4});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/index.php/apps/deck/#/board/4/card/0"), new long[]{4});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/index.php/apps/deck/#/board/4/foo"), new long[]{4});
+        assertArrayEquals(new long[]{4}, ProjectUtil.extractBoardIdAndCardIdFromUrl("index.php/apps/deck/#/board/4"));
+        assertArrayEquals(new long[]{4}, ProjectUtil.extractBoardIdAndCardIdFromUrl("/index.php/apps/deck/#/board/4"));
+        assertArrayEquals(new long[]{4}, ProjectUtil.extractBoardIdAndCardIdFromUrl("example.com/index.php/apps/deck/#/board/4"));
+        assertArrayEquals(new long[]{4}, ProjectUtil.extractBoardIdAndCardIdFromUrl("http://example.com/index.php/apps/deck/#/board/4"));
+        assertArrayEquals(new long[]{4}, ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/index.php/apps/deck/#/board/4"));
+        assertArrayEquals(new long[]{4}, ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/index.php/apps/deck/#/board/4"));
+        assertArrayEquals(new long[]{4}, ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/index.php/apps/deck/#/board/4/card"));
+        assertArrayEquals(new long[]{4}, ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/index.php/apps/deck/#/board/4/card/"));
+        assertArrayEquals(new long[]{4}, ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/index.php/apps/deck/#/board/4/card/0"));
+        assertArrayEquals(new long[]{4}, ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/index.php/apps/deck/#/board/4/foo"));
 
         // Valid board URLs with # and without index.php
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("apps/deck/#/board/4"), new long[]{4});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("/apps/deck/#/board/4"), new long[]{4});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("example.com/apps/deck/#/board/4"), new long[]{4});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("http://example.com/apps/deck/#/board/4"), new long[]{4});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/apps/deck/#/board/4"), new long[]{4});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/apps/deck/#/board/4"), new long[]{4});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/apps/deck/#/board/4/card"), new long[]{4});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/apps/deck/#/board/4/card/"), new long[]{4});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/apps/deck/#/board/4/card/0"), new long[]{4});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/apps/deck/#/board/4/foo"), new long[]{4});
+        assertArrayEquals(new long[]{4}, ProjectUtil.extractBoardIdAndCardIdFromUrl("apps/deck/#/board/4"));
+        assertArrayEquals(new long[]{4}, ProjectUtil.extractBoardIdAndCardIdFromUrl("/apps/deck/#/board/4"));
+        assertArrayEquals(new long[]{4}, ProjectUtil.extractBoardIdAndCardIdFromUrl("example.com/apps/deck/#/board/4"));
+        assertArrayEquals(new long[]{4}, ProjectUtil.extractBoardIdAndCardIdFromUrl("http://example.com/apps/deck/#/board/4"));
+        assertArrayEquals(new long[]{4}, ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/apps/deck/#/board/4"));
+        assertArrayEquals(new long[]{4}, ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/apps/deck/#/board/4"));
+        assertArrayEquals(new long[]{4}, ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/apps/deck/#/board/4/card"));
+        assertArrayEquals(new long[]{4}, ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/apps/deck/#/board/4/card/"));
+        assertArrayEquals(new long[]{4}, ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/apps/deck/#/board/4/card/0"));
+        assertArrayEquals(new long[]{4}, ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/apps/deck/#/board/4/foo"));
 
         // Valid board URLs without # and with index.php
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("index.php/apps/deck/board/4"), new long[]{4});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("/index.php/apps/deck/board/4"), new long[]{4});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("example.com/index.php/apps/deck/board/4"), new long[]{4});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("http://example.com/index.php/apps/deck/board/4"), new long[]{4});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/index.php/apps/deck/board/4"), new long[]{4});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/index.php/apps/deck/board/4"), new long[]{4});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/index.php/apps/deck/board/4/card"), new long[]{4});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/index.php/apps/deck/board/4/card/"), new long[]{4});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/index.php/apps/deck/board/4/card/0"), new long[]{4});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/index.php/apps/deck/board/4/foo"), new long[]{4});
+        assertArrayEquals(new long[]{4}, ProjectUtil.extractBoardIdAndCardIdFromUrl("index.php/apps/deck/board/4"));
+        assertArrayEquals(new long[]{4}, ProjectUtil.extractBoardIdAndCardIdFromUrl("/index.php/apps/deck/board/4"));
+        assertArrayEquals(new long[]{4}, ProjectUtil.extractBoardIdAndCardIdFromUrl("example.com/index.php/apps/deck/board/4"));
+        assertArrayEquals(new long[]{4}, ProjectUtil.extractBoardIdAndCardIdFromUrl("http://example.com/index.php/apps/deck/board/4"));
+        assertArrayEquals(new long[]{4}, ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/index.php/apps/deck/board/4"));
+        assertArrayEquals(new long[]{4}, ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/index.php/apps/deck/board/4"));
+        assertArrayEquals(new long[]{4}, ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/index.php/apps/deck/board/4/card"));
+        assertArrayEquals(new long[]{4}, ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/index.php/apps/deck/board/4/card/"));
+        assertArrayEquals(new long[]{4}, ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/index.php/apps/deck/board/4/card/0"));
+        assertArrayEquals(new long[]{4}, ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/index.php/apps/deck/board/4/foo"));
 
         // Valid board URLs without # and without index.php
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("apps/deck/board/4"), new long[]{4});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("/apps/deck/board/4"), new long[]{4});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("example.com/apps/deck/board/4"), new long[]{4});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("http://example.com/apps/deck/board/4"), new long[]{4});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/apps/deck/board/4"), new long[]{4});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/apps/deck/board/4"), new long[]{4});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/apps/deck/board/4/card"), new long[]{4});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/apps/deck/board/4/card/"), new long[]{4});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/apps/deck/board/4/card/0"), new long[]{4});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/apps/deck/board/4/foo"), new long[]{4});
+        assertArrayEquals(new long[]{4}, ProjectUtil.extractBoardIdAndCardIdFromUrl("apps/deck/board/4"));
+        assertArrayEquals(new long[]{4}, ProjectUtil.extractBoardIdAndCardIdFromUrl("/apps/deck/board/4"));
+        assertArrayEquals(new long[]{4}, ProjectUtil.extractBoardIdAndCardIdFromUrl("example.com/apps/deck/board/4"));
+        assertArrayEquals(new long[]{4}, ProjectUtil.extractBoardIdAndCardIdFromUrl("http://example.com/apps/deck/board/4"));
+        assertArrayEquals(new long[]{4}, ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/apps/deck/board/4"));
+        assertArrayEquals(new long[]{4}, ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/apps/deck/board/4"));
+        assertArrayEquals(new long[]{4}, ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/apps/deck/board/4/card"));
+        assertArrayEquals(new long[]{4}, ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/apps/deck/board/4/card/"));
+        assertArrayEquals(new long[]{4}, ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/apps/deck/board/4/card/0"));
+        assertArrayEquals(new long[]{4}, ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/apps/deck/board/4/foo"));
 
         // Valid card URLs with # and with index.php
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("index.php/apps/deck/#/board/4/card/6"), new long[]{4, 6});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("/index.php/apps/deck/#/board/4/card/6"), new long[]{4, 6});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("example.com/index.php/apps/deck/#/board/4/card/6"), new long[]{4, 6});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("http://example.com/index.php/apps/deck/#/board/4/card/6"), new long[]{4, 6});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/index.php/apps/deck/#/board/4/card/6"), new long[]{4, 6});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/index.php/apps/deck/#/board/4/card/6/"), new long[]{4, 6});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/index.php/apps/deck/#/board/4/card/6"), new long[]{4, 6});
+        assertArrayEquals(new long[]{4, 6}, ProjectUtil.extractBoardIdAndCardIdFromUrl("index.php/apps/deck/#/board/4/card/6"));
+        assertArrayEquals(new long[]{4, 6}, ProjectUtil.extractBoardIdAndCardIdFromUrl("/index.php/apps/deck/#/board/4/card/6"));
+        assertArrayEquals(new long[]{4, 6}, ProjectUtil.extractBoardIdAndCardIdFromUrl("example.com/index.php/apps/deck/#/board/4/card/6"));
+        assertArrayEquals(new long[]{4, 6}, ProjectUtil.extractBoardIdAndCardIdFromUrl("http://example.com/index.php/apps/deck/#/board/4/card/6"));
+        assertArrayEquals(new long[]{4, 6}, ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/index.php/apps/deck/#/board/4/card/6"));
+        assertArrayEquals(new long[]{4, 6}, ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/index.php/apps/deck/#/board/4/card/6/"));
+        assertArrayEquals(new long[]{4, 6}, ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/index.php/apps/deck/#/board/4/card/6"));
 
         // Valid card URLs with # and without index.php
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("apps/deck/#/board/4/card/6"), new long[]{4, 6});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("/apps/deck/#/board/4/card/6"), new long[]{4, 6});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("example.com/apps/deck/#/board/4/card/6"), new long[]{4, 6});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("http://example.com/apps/deck/#/board/4/card/6"), new long[]{4, 6});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/apps/deck/#/board/4/card/6"), new long[]{4, 6});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/apps/deck/#/board/4/card/6/"), new long[]{4, 6});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/apps/deck/#/board/4/card/6"), new long[]{4, 6});
+        assertArrayEquals(new long[]{4, 6}, ProjectUtil.extractBoardIdAndCardIdFromUrl("apps/deck/#/board/4/card/6"));
+        assertArrayEquals(new long[]{4, 6}, ProjectUtil.extractBoardIdAndCardIdFromUrl("/apps/deck/#/board/4/card/6"));
+        assertArrayEquals(new long[]{4, 6}, ProjectUtil.extractBoardIdAndCardIdFromUrl("example.com/apps/deck/#/board/4/card/6"));
+        assertArrayEquals(new long[]{4, 6}, ProjectUtil.extractBoardIdAndCardIdFromUrl("http://example.com/apps/deck/#/board/4/card/6"));
+        assertArrayEquals(new long[]{4, 6}, ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/apps/deck/#/board/4/card/6"));
+        assertArrayEquals(new long[]{4, 6}, ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/apps/deck/#/board/4/card/6/"));
+        assertArrayEquals(new long[]{4, 6}, ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/apps/deck/#/board/4/card/6"));
 
         // Valid card URLs without # and with index.php
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("index.php/apps/deck/#/board/4/card/6"), new long[]{4, 6});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("/index.php/apps/deck/#/board/4/card/6"), new long[]{4, 6});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("example.com/index.php/apps/deck/#/board/4/card/6"), new long[]{4, 6});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("http://example.com/index.php/apps/deck/#/board/4/card/6"), new long[]{4, 6});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/index.php/apps/deck/#/board/4/card/6"), new long[]{4, 6});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/index.php/apps/deck/#/board/4/card/6/"), new long[]{4, 6});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/index.php/apps/deck/#/board/4/card/6"), new long[]{4, 6});
+        assertArrayEquals(new long[]{4, 6}, ProjectUtil.extractBoardIdAndCardIdFromUrl("index.php/apps/deck/#/board/4/card/6"));
+        assertArrayEquals(new long[]{4, 6}, ProjectUtil.extractBoardIdAndCardIdFromUrl("/index.php/apps/deck/#/board/4/card/6"));
+        assertArrayEquals(new long[]{4, 6}, ProjectUtil.extractBoardIdAndCardIdFromUrl("example.com/index.php/apps/deck/#/board/4/card/6"));
+        assertArrayEquals(new long[]{4, 6}, ProjectUtil.extractBoardIdAndCardIdFromUrl("http://example.com/index.php/apps/deck/#/board/4/card/6"));
+        assertArrayEquals(new long[]{4, 6}, ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/index.php/apps/deck/#/board/4/card/6"));
+        assertArrayEquals(new long[]{4, 6}, ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/index.php/apps/deck/#/board/4/card/6/"));
+        assertArrayEquals(new long[]{4, 6}, ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/index.php/apps/deck/#/board/4/card/6"));
 
         // Valid card URLs without # and without index.php
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("apps/deck/#/board/4/card/6"), new long[]{4, 6});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("/apps/deck/#/board/4/card/6"), new long[]{4, 6});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("example.com/apps/deck/#/board/4/card/6"), new long[]{4, 6});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("http://example.com/apps/deck/#/board/4/card/6"), new long[]{4, 6});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/apps/deck/#/board/4/card/6"), new long[]{4, 6});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/apps/deck/#/board/4/card/6/"), new long[]{4, 6});
-        assertArrayEquals(ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/apps/deck/#/board/4/card/6"), new long[]{4, 6});
+        assertArrayEquals(new long[]{4, 6}, ProjectUtil.extractBoardIdAndCardIdFromUrl("apps/deck/#/board/4/card/6"));
+        assertArrayEquals(new long[]{4, 6}, ProjectUtil.extractBoardIdAndCardIdFromUrl("/apps/deck/#/board/4/card/6"));
+        assertArrayEquals(new long[]{4, 6}, ProjectUtil.extractBoardIdAndCardIdFromUrl("example.com/apps/deck/#/board/4/card/6"));
+        assertArrayEquals(new long[]{4, 6}, ProjectUtil.extractBoardIdAndCardIdFromUrl("http://example.com/apps/deck/#/board/4/card/6"));
+        assertArrayEquals(new long[]{4, 6}, ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/apps/deck/#/board/4/card/6"));
+        assertArrayEquals(new long[]{4, 6}, ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/apps/deck/#/board/4/card/6/"));
+        assertArrayEquals(new long[]{4, 6}, ProjectUtil.extractBoardIdAndCardIdFromUrl("https://example.com/nextcloud/apps/deck/#/board/4/card/6"));
 
         // URLs to talk
         assertThrows(IllegalArgumentException.class, () -> ProjectUtil.extractBoardIdAndCardIdFromUrl("index.php/call/qkzhe5k2"));
