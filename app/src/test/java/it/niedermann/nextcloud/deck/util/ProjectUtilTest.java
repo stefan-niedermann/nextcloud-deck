ed4192050b9be043e02d3f475cc186dbbd206420
Stefan Niedermann <info@niedermann.it>
2021-10-16 10:40:46 +0200

    Set Robolectric SDK explicitly
    Signed-off-by: Stefan Niedermann <info@niedermann.it>

--- app/src/test/java/it/niedermann/nextcloud/deck/util/ProjectUtilTest.java
+++ app/src/test/java/it/niedermann/nextcloud/deck/util/ProjectUtilTest.java
@@ -1,11 +1,9 @@
-package it.niedermann.nextcloud.deck;
-
-import org.junit.Test;
-
-import it.niedermann.nextcloud.deck.util.ProjectUtil;
+package it.niedermann.nextcloud.deck.util;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertThrows;
+
+import org.junit.Test;
 
 public class ProjectUtilTest {
     @Test
