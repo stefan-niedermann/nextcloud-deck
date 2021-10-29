e7715e5d3a7f92a24048d8521f37191236402189
Stefan Niedermann <info@niedermann.it>
2021-10-29 22:40:39 +0200

    Configure NC account via adb
    Signed-off-by: Stefan Niedermann <info@niedermann.it>

--- app/src/androidTest/java/it/niedermann/nextcloud/deck/ui/E2ETest.java
+++ app/src/androidTest/java/it/niedermann/nextcloud/deck/ui/E2ETest.java
@@ -1,8 +1,10 @@
 package it.niedermann.nextcloud.deck.ui;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.filters.SmallTest;
-import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Rule;
@@ -21,15 +23,7 @@ public class E2ETest {
     public void sampleTest() {
         final var scenario = rule.getScenario();
         scenario.onActivity(activity -> {
-            activity.binding.addButton.performClick();
-
-            final var inst = InstrumentationRegistry.getInstrumentation();
-            inst.getUiAutomation().executeShellCommand("input keyevent \"KEYCODE_TAB\"");
-            inst.getUiAutomation().executeShellCommand("input keyevent \"KEYCODE_SPACE\"");
-            inst.getUiAutomation().executeShellCommand("input keyevent \"KEYCODE_TAB\"");
-            inst.getUiAutomation().executeShellCommand("input keyevent \"KEYCODE_TAB\"");
-            inst.getUiAutomation().executeShellCommand("input keyevent \"KEYCODE_TAB\"");
-            inst.getUiAutomation().executeShellCommand("input keyevent \"KEYCODE_ENTER\"");
+            onView(withText("Task"));
         });
     }
 }
