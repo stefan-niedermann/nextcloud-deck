8c40b696b489cc276d23e6e725815274054e94cd
Stefan Niedermann <info@niedermann.it>
2021-10-29 22:34:48 +0200

    Fix package name of downloaded NC apk
    Signed-off-by: Stefan Niedermann <info@niedermann.it>

--- app/src/androidTest/java/it/niedermann/nextcloud/deck/ui/E2ETest.java
+++ app/src/androidTest/java/it/niedermann/nextcloud/deck/ui/E2ETest.java
@@ -1,17 +1,13 @@
 package it.niedermann.nextcloud.deck.ui;
 
-import android.app.Instrumentation;
-import android.view.KeyEvent;
-
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.filters.SmallTest;
+import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import java.util.concurrent.CountDownLatch;
 
 // @RunWith is required only if you use a mix of JUnit3 and JUnit4.
 @RunWith(AndroidJUnit4.class)
@@ -25,33 +21,15 @@ public class E2ETest {
     public void sampleTest() {
         final var scenario = rule.getScenario();
         scenario.onActivity(activity -> {
-            final var latch = new CountDownLatch(1);
             activity.binding.addButton.performClick();
-            new Thread(() -> {
-                try {
-                    final var inst = new Instrumentation();
-                    Thread.sleep(1_000);
-                    inst.sendKeyDownUpSync(KeyEvent.KEYCODE_TAB);
-                    Thread.sleep(1_000);
-                    inst.sendKeyDownUpSync(KeyEvent.KEYCODE_SPACE);
-                    Thread.sleep(1_000);
-                    inst.sendKeyDownUpSync(KeyEvent.KEYCODE_TAB);
-                    Thread.sleep(1_000);
-                    inst.sendKeyDownUpSync(KeyEvent.KEYCODE_TAB);
-                    Thread.sleep(1_000);
-                    inst.sendKeyDownUpSync(KeyEvent.KEYCODE_TAB);
-                    Thread.sleep(1_000);
-                    inst.sendKeyDownUpSync(KeyEvent.KEYCODE_ENTER);
-                    latch.countDown();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }).start();
-            try {
-                latch.await();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+
+            final var inst = InstrumentationRegistry.getInstrumentation();
+            inst.getUiAutomation().executeShellCommand("input keyevent \"KEYCODE_TAB\"");
+            inst.getUiAutomation().executeShellCommand("input keyevent \"KEYCODE_SPACE\"");
+            inst.getUiAutomation().executeShellCommand("input keyevent \"KEYCODE_TAB\"");
+            inst.getUiAutomation().executeShellCommand("input keyevent \"KEYCODE_TAB\"");
+            inst.getUiAutomation().executeShellCommand("input keyevent \"KEYCODE_TAB\"");
+            inst.getUiAutomation().executeShellCommand("input keyevent \"KEYCODE_ENTER\"");
         });
     }
 }
