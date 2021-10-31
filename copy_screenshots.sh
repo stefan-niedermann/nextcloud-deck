f715c107dc6203a78ffa3257365fb55f50841fca
Stefan Niedermann <info@niedermann.it>
2021-10-31 13:42:40 +0100

    adb ls
    Signed-off-by: Stefan Niedermann <info@niedermann.it>

--- copy_screenshots.sh
+++ copy_screenshots.sh
@@ -4,12 +4,18 @@ adb logcat -c
 adb logcat *:E -v color &
 if ./gradlew connectedDebugAndroidTest; then
   echo "connectedDebugAndroidTest succeeded" >&2
+  echo "adb ls -all (1)"
+  adb shell ls -R /
   echo "ls -all (1)"
   ls -all
   adb pull /sdcard/screenshots screenshots
+  echo "adb ls -all (1.1)"
+  adb shell ls -R /
   echo "ls -all (1.1)"
   ls -all
   adb exec-out /sdcard/screenshots -p >screenshots
+  echo "adb ls -all (1.2)"
+  adb shell ls -R /
   echo "ls -all (1.2)"
   ls -all
   exit 0
