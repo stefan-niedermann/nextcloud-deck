0f1923b0311df20553c60bfd50e63ce22333727b
Stefan Niedermann <info@niedermann.it>
2021-10-31 14:05:51 +0100

    Do not adb shell ls recursively
    Signed-off-by: Stefan Niedermann <info@niedermann.it>

--- copy_screenshots.sh
+++ copy_screenshots.sh
@@ -5,22 +5,25 @@ adb logcat *:E -v color &
 if ./gradlew connectedDebugAndroidTest; then
   echo "connectedDebugAndroidTest succeeded" >&2
   echo "adb ls -all (1)"
-  adb shell ls -R /
-  adb shell ls -R /sdcard/
-  adb shell ls -R /sdcard/screenshots
+  adb shell ls /
+  adb shell ls /mnt/
+  adb shell ls /mnt/sdcard
+  adb shell ls -R /mnt/sdcard/screenshots
   echo "ls -all (1)"
   ls -all
   adb pull /sdcard/screenshots screenshots
   echo "adb ls -all (1.1)"
-  adb shell ls -R /
-  adb shell ls -R /sdcard/
+  adb shell ls /
+  adb shell ls /mnt/
+  adb shell ls /mnt/sdcard
   adb shell ls -R /sdcard/screenshots
   echo "ls -all (1.1)"
   ls -all
   adb exec-out /sdcard/screenshots -p >screenshots
   echo "adb ls -all (1.2)"
-  adb shell ls -R /
-  adb shell ls -R /sdcard/
+  adb shell ls /
+  adb shell ls /mnt/
+  adb shell ls /mnt/sdcard
   adb shell ls -R /sdcard/screenshots
   echo "ls -all (1.2)"
   ls -all
