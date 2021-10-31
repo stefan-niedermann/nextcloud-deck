ea189efadcdba7b3d47ccf57ae4ee5910719d6f1
Stefan Niedermann <info@niedermann.it>
2021-10-31 13:43:14 +0100

    More output
    Signed-off-by: Stefan Niedermann <info@niedermann.it>

--- copy_screenshots.sh
+++ copy_screenshots.sh
@@ -6,16 +6,22 @@ if ./gradlew connectedDebugAndroidTest; then
   echo "connectedDebugAndroidTest succeeded" >&2
   echo "adb ls -all (1)"
   adb shell ls -R /
+  adb shell ls -R /sdcard/
+  adb shell ls -R /sdcard/screenshots
   echo "ls -all (1)"
   ls -all
   adb pull /sdcard/screenshots screenshots
   echo "adb ls -all (1.1)"
   adb shell ls -R /
+  adb shell ls -R /sdcard/
+  adb shell ls -R /sdcard/screenshots
   echo "ls -all (1.1)"
   ls -all
   adb exec-out /sdcard/screenshots -p >screenshots
   echo "adb ls -all (1.2)"
   adb shell ls -R /
+  adb shell ls -R /sdcard/
+  adb shell ls -R /sdcard/screenshots
   echo "ls -all (1.2)"
   ls -all
   exit 0
