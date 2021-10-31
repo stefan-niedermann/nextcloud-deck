abe5dd239d6460ffe9e98339a50bd049ab4e57a7
Stefan Niedermann <info@niedermann.it>
2021-10-31 09:43:17 +0100

    Try new e2e test using UI Automator
    Signed-off-by: Stefan Niedermann <info@niedermann.it>

--- app/src/androidTest/java/it/niedermann/nextcloud/deck/ui/E2ETest.java
+++ app/src/androidTest/java/it/niedermann/nextcloud/deck/ui/E2ETest.java
@@ -1,40 +1,135 @@
 package it.niedermann.nextcloud.deck.ui;
 
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 
-import androidx.test.core.app.ActivityScenario;
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import android.content.Context;
+import android.content.Intent;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
 
-import org.junit.After;
-import org.junit.Rule;
+import androidx.test.uiautomator.By;
+import androidx.test.uiautomator.UiDevice;
+import androidx.test.uiautomator.UiObjectNotFoundException;
+import androidx.test.uiautomator.UiSelector;
+import androidx.test.uiautomator.Until;
+
+import com.google.android.material.card.MaterialCardView;
+
 import org.junit.Test;
-
-import it.niedermann.nextcloud.deck.R;
 
 public class E2ETest {
 
-    public ActivityScenario<MainActivity> scenario;
-    @Rule
-    public ActivityScenarioRule<MainActivity> rule = new ActivityScenarioRule<>(MainActivity.class);
+    private UiDevice mDevice;
 
     @Test
-    public void sampleTest() {
-        scenario = rule.getScenario();
-        scenario.onActivity(activity -> {
-            final var fieldMatcher = withId(R.id.filter);
-            final var interaction = onView(fieldMatcher);
-            final var matcher = isDisplayed();
-            final var assertion = matches(matcher);
-//            interaction.check(assertion);
-//            onView(withText("Halloween")).check(matches(isDisplayed()));
-        });
+    public void setUp() throws UiObjectNotFoundException {
+        setupNextcloudAccount("http://localhost:8080", "Test", "Test");
+        importAccountIntoDeck();
+        verfiyCardsPresent();
     }
 
-    @After
-    public void cleanup() {
-        scenario.close();
+    private void setupNextcloudAccount(String url, String username, String password) throws UiObjectNotFoundException {
+        final var CALC_PACKAGE = "com.nextcloud.android.beta";
+        // Initialize UiDevice instance
+        mDevice = UiDevice.getInstance(getInstrumentation());
+
+        // Launch a simple calculator app
+        final var context = getInstrumentation().getContext();
+        final var intent = context.getPackageManager()
+                .getLaunchIntentForPackage(CALC_PACKAGE)
+                .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        // Clear out any previous instances
+        context.startActivity(intent);
+        mDevice.wait(Until.hasObject(By.pkg(CALC_PACKAGE).depth(0)), 30);
+
+        mDevice.findObject(new UiSelector().text("Log in")).click();
+        mDevice.findObject(new UiSelector().focused(true)).setText(url);
+        mDevice.pressEnter();
+        mDevice.findObject(new UiSelector().text("Log in")).click();
+
+        mDevice.wait(Until.findObject(By.clazz(WebView.class)), 30);
+
+        final var usernameInput = mDevice.findObject(new UiSelector()
+                .instance(0)
+                .className(EditText.class));
+
+        usernameInput.waitForExists(30);
+        usernameInput.setText(username);
+
+        final var passwordInput = mDevice.findObject(new UiSelector()
+                .instance(1)
+                .className(EditText.class));
+
+        passwordInput.waitForExists(30);
+        passwordInput.setText(password);
+
+        mDevice.findObject(new UiSelector().text("Log in")).click();
+
+        mDevice.findObject(new UiSelector().text("Grant access")).click();
+    }
+
+    private void importAccountIntoDeck() throws UiObjectNotFoundException {
+        final var CALC_PACKAGE = "it.niedermann.nextcloud.deck.dev";
+        // Initialize UiDevice instance
+        mDevice = UiDevice.getInstance(getInstrumentation());
+
+        // Launch a simple calculator app
+        Context context = getInstrumentation().getContext();
+        Intent intent = context.getPackageManager()
+                .getLaunchIntentForPackage(CALC_PACKAGE);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        // Clear out any previous instances
+        context.startActivity(intent);
+        mDevice.wait(Until.hasObject(By.pkg(CALC_PACKAGE).depth(0)), 30);
+
+        final var accountButton = mDevice.findObject(new UiSelector()
+                .instance(0)
+                .className(Button.class));
+
+        accountButton.waitForExists(30);
+        accountButton.click();
+
+        final var radioAccount = mDevice.findObject(new UiSelector()
+                .clickable(true)
+                .instance(0));
+
+        radioAccount.waitForExists(30);
+        radioAccount.click();
+
+        final var okButton = mDevice.findObject(new UiSelector().text("OK"));
+
+        okButton.waitForExists(30);
+        okButton.click();
+
+        final var allowButton = mDevice.findObject(new UiSelector().text("Allow"));
+
+        allowButton.waitForExists(30);
+        allowButton.click();
+
+        final var welcomeText = mDevice.findObject(new UiSelector().description("Filter"));
+        welcomeText.waitForExists(30);
+//        mDevice.wait
+    }
+
+    private void verfiyCardsPresent() {
+        final var CALC_PACKAGE = "it.niedermann.nextcloud.deck.dev";
+        // Initialize UiDevice instance
+        mDevice = UiDevice.getInstance(getInstrumentation());
+
+        // Launch a simple calculator app
+        Context context = getInstrumentation().getContext();
+        Intent intent = context.getPackageManager()
+                .getLaunchIntentForPackage(CALC_PACKAGE);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        // Clear out any previous instances
+        context.startActivity(intent);
+        mDevice.wait(Until.hasObject(By.pkg(CALC_PACKAGE).depth(0)), 30);
+
+        final var accountButton = mDevice.findObject(new UiSelector()
+                .instance(0)
+                .className(MaterialCardView.class));
+
+        accountButton.waitForExists(30);
     }
 }
