f3d6553c41b6eec6ba05589cf2ff57fee690c549
Stefan Niedermann <info@niedermann.it>
2021-10-31 12:49:01 +0100

    More verbose
    Signed-off-by: Stefan Niedermann <info@niedermann.it>

--- copy_screenshots.sh
+++ copy_screenshots.sh
@@ -4,8 +4,23 @@ adb logcat -c
 adb logcat *:E -v color &
 if ./gradlew connectedDebugAndroidTest; then
   echo "connectedDebugAndroidTest succeeded" >&2
-else
+  echo "ls -all (1)"
+  ls -all
   adb pull /sdcard/screenshots screenshots
+  echo "ls -all (1.1)"
+  ls -all
   adb exec-out /sdcard/screenshots -p >screenshots
+  echo "ls -all (1.2)"
+  ls -all
+  exit 0
+else
+  echo "ls -all (2)"
+  ls -all
+  adb pull /sdcard/screenshots screenshots
+  echo "ls -all (2.1)"
+  ls -all
+  adb exec-out /sdcard/screenshots -p >screenshots
+  echo "ls -all (2.2)"
+  ls -all
   exit 1
 fi
