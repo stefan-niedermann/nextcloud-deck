f941c50abcd85fbab498e520d65dc6447f2c1f6c
Stefan Niedermann <info@niedermann.it>
2021-11-02 15:08:54 +0100

    Add more verbose logging
    Signed-off-by: Stefan Niedermann <info@niedermann.it>

--- app/src/androidTest/java/it/niedermann/nextcloud/deck/ui/E2ETest.java
+++ app/src/androidTest/java/it/niedermann/nextcloud/deck/ui/E2ETest.java
@@ -30,6 +30,8 @@ import java.net.URL;
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class E2ETest {
 
+    private static final String TAG = E2ETest.class.getSimpleName();
+
     private UiDevice mDevice;
 
     private static final int TIMEOUT = 10_000;
@@ -70,38 +72,55 @@ public class E2ETest {
                 content.append(inputLine);
             }
             in.close();
-            Log.i("TRC", content.toString());
+            Log.i(TAG, content.toString());
         } catch (IOException e) {
-            Log.e("TRC", "REQUEST FAILED", e);
+            Log.e(TAG, "REQUEST FAILED", e);
         }
 
-        Log.e("TRC", "FIRST");
+        Log.e(TAG, "FIRST");
         final var loginButton = mDevice.findObject(new UiSelector().textContains("Log in"));
         loginButton.waitForExists(TIMEOUT);
-        Log.e("TRC", "LOGINBUTTON EXISTS");
+        Log.e(TAG, "LOGINBUTTON EXISTS. CLICKING ON IT...");
         loginButton.click();
+        Log.e(TAG, "LOGINBUTTON CLICKED");
 
-        mDevice.findObject(new UiSelector().focused(true)).setText(SERVER_URL);
-        Log.e("TRC", "PRESSING NOW ENTER...");
+        final var urlInput = mDevice.findObject(new UiSelector().focused(true));
+        urlInput.waitForExists(TIMEOUT);
+        Log.e(TAG, "URL INPUT IS PRESENT");
+        Log.e(TAG, "ENTERING URL...");
+        urlInput.setText(SERVER_URL);
+        Log.e(TAG, "URL ENTERED.");
+
+        Log.e(TAG, "PRESSING NOW ENTER...");
         mDevice.pressEnter();
-        Log.e("TRC", "SECOND");
-        mDevice.findObject(new UiSelector().text("Log in")).click();
-        Log.e("TRC", "WAITING FOR WEBVIEW");
+        Log.e(TAG, "ENTER PRESSED.");
+        final var webViewLoginButton = mDevice.findObject(new UiSelector().text("Log in"));
+        Log.e(TAG, "WAITING FOR WEBVIEW LOGIN BUTTON TO BE PRESENT...");
+        webViewLoginButton.waitForExists(TIMEOUT);
+        Log.e(TAG, "WEBVIEW LOGIN BUTTON IS PRESENT. CLICKING ON IT...");
+        webViewLoginButton.click();
+        Log.e(TAG, "WAITING FOR WEBVIEW");
         mDevice.wait(Until.findObject(By.clazz(WebView.class)), TIMEOUT);
+        Log.e(TAG, "WEBVIEW IS PRESENT");
 
         final var usernameInput = mDevice.findObject(new UiSelector()
                 .instance(0)
                 .className(EditText.class));
+        Log.e(TAG, "WAITING FOR USERNAME INPUT TO BE PRESENT...");
         usernameInput.waitForExists(TIMEOUT);
+        Log.e(TAG, "USERNAME INPUT IS PRESENT. SETTING TEXT...");
         usernameInput.setText(SERVER_USERNAME);
+        Log.e(TAG, "USERNAME HAS BEEN SET.");
 
         final var passwordInput = mDevice.findObject(new UiSelector()
                 .instance(1)
                 .className(EditText.class));
+        Log.e(TAG, "WAITING FOR USERNAME INPUT TO BE PRESENT...");
         passwordInput.waitForExists(TIMEOUT);
+        Log.e(TAG, "USERNAME INPUT IS PRESENT. SETTING TEXT...");
         passwordInput.setText(SERVER_PASSWORD);
 
-        Log.e("TRC", "THIRD");
+        Log.e(TAG, "THIRD");
         mDevice.findObject(new UiSelector().text("Log in")).click();
         mDevice.findObject(new UiSelector().text("Grant access")).click();
     }
