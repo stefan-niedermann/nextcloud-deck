1efc1886f74f223e80f7fa5ec0007fd643bff262
Stefan Niedermann <info@niedermann.it>
2022-01-17 12:31:59 +0100

    Make ServerCommunicationErrorHandler more generic
    Signed-off-by: Stefan Niedermann <info@niedermann.it>

--- app/src/main/java/it/niedermann/nextcloud/deck/api/ServerCommunicationErrorHandler.java
+++ app/src/main/java/it/niedermann/nextcloud/deck/api/ServerCommunicationErrorHandler.java
@@ -4,38 +4,51 @@ import androidx.annotation.NonNull;
 
 import com.nextcloud.android.sso.exceptions.UnknownErrorException;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Objects;
 
 import it.niedermann.nextcloud.deck.exceptions.OfflineException;
 
 public class ServerCommunicationErrorHandler {
+
+    private static final Handler[] handlers = new Handler[]{
+            new Handler(UnknownErrorException.class, Arrays.asList("econnrefused", "connection refused"), OfflineException.Reason.CONNECTION_REFUSED),
+            new Handler(ClassNotFoundException.class, Collections.singletonList("connecttimeoutexception"), OfflineException.Reason.CONNECTION_TIMEOUT)
+    };
+
     public static Throwable translateError(Throwable error) {
         try {
-            if (error.getClass() == UnknownErrorException.class) {
-                return handleSsoExceptions((UnknownErrorException) error);
-            } else if (error.getClass() == ClassNotFoundException.class) {
-                return handleClassNotFoundError((ClassNotFoundException) error);
-            } else {
-                return error;
+            for (final var handler : handlers) {
+                if (error.getClass() == handler.originalExceptionType) {
+                    return handler.handle(error);
+                }
             }
+            return error;
         } catch (NullPointerException e) {
             return error;
         }
     }
 
-    private static Throwable handleSsoExceptions(@NonNull UnknownErrorException error) {
-        final String message = Objects.requireNonNull(error.getMessage(), "SSO handler got no ExceptionMessage").toLowerCase();
-        if (message.contains("econnrefused") || message.contains("connection refused")) {
-            return new OfflineException(OfflineException.Reason.CONNECTION_REFUSED);
-        }
-        return error;
-    }
+    private static class Handler {
+        private final Class<?> originalExceptionType;
+        private final Collection<String> indicators;
+        private final OfflineException.Reason reason;
 
-    private static Throwable handleClassNotFoundError(@NonNull ClassNotFoundException error) {
-        final String message = Objects.requireNonNull(error.getMessage(), "ClassNotFound handler got no ExceptionMessage").toLowerCase();
-        if (message.contains("connecttimeoutexception")) {
-            return new OfflineException(OfflineException.Reason.CONNECTION_TIMEOUT);
+        Handler(@NonNull Class<?> originalExceptionType, @NonNull Collection<String> indicators, @NonNull OfflineException.Reason reason) {
+            this.originalExceptionType = originalExceptionType;
+            this.indicators = indicators;
+            this.reason = reason;
         }
-        return error;
+
+        @NonNull
+        Throwable handle(@NonNull Throwable error) throws NullPointerException {
+            final String message = Objects.requireNonNull(error.getMessage(), "ExceptionMessage is null").toLowerCase();
+            if (indicators.stream().anyMatch(message::contains)) {
+                return new OfflineException(reason);
+            }
+            return error;
+        }
     }
 }
