348f51e38de84813ba813ec510d8427830c76135
Stefan Niedermann <info@niedermann.it>
2023-05-23 08:40:09 +0200

    chore(deps): Update to SSO 0.8.0
    Signed-off-by: Stefan Niedermann <info@niedermann.it>

--- app/src/main/java/it/niedermann/nextcloud/deck/remote/api/ApiProvider.java
+++ app/src/main/java/it/niedermann/nextcloud/deck/remote/api/ApiProvider.java
@@ -30,7 +30,7 @@ public class ApiProvider {
 
     public synchronized void initSsoApi(@NonNull final NextcloudAPI.ApiConnectedListener callback) {
         if (this.deckAPI == null) {
-            final NextcloudAPI nextcloudAPI = new NextcloudAPI(context, ssoAccount, GsonConfig.getGson(), callback);
+            final var nextcloudAPI = new NextcloudAPI(context, ssoAccount, GsonConfig.getGson(), callback);
             this.deckAPI = new NextcloudRetrofitApiBuilder(nextcloudAPI, DECK_API_ENDPOINT).create(DeckAPI.class);
             this.nextcloudAPI = new NextcloudRetrofitApiBuilder(nextcloudAPI, NC_API_ENDPOINT).create(NextcloudServerAPI.class);
         }
