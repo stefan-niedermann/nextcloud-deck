35f94be61a18013a42cd5b1d1c2955b28a26d405
desperateCoder <echotodevnull@gmail.com>
2024-01-15 12:47:19 +0100

    #1556 mark card as done: reset etags for update

--- app/src/main/java/it/niedermann/nextcloud/deck/database/migration/Migration_32_33.java
+++ app/src/main/java/it/niedermann/nextcloud/deck/database/migration/Migration_32_33.java
@@ -17,6 +17,9 @@ public class Migration_32_33 extends Migration {
     public void migrate(@NonNull SupportSQLiteDatabase database) {
         database.execSQL("ALTER TABLE `Card` add column done INTEGER");
         // reset etags, so cards will be fetched after app-update
-        database.execSQL("UPDATE `Card` SET etag = NULL");
+        database.execSQL("UPDATE `Account` SET `boardsEtag` = NULL");
+        database.execSQL("UPDATE `Board` SET `etag` = NULL");
+        database.execSQL("UPDATE `Stack` SET `etag` = NULL");
+        database.execSQL("UPDATE `Card` SET `etag` = NULL");
     }
 }
