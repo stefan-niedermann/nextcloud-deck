3e7a17250ae15e34a7e98435126678ae397df182
desperateCoder <echotodevnull@gmail.com>
2024-11-24 20:39:40 +0100

    #1503 dupicates on upsync: assignment/unassignment of groups -> make it more failsafe

--- app/src/main/java/it/niedermann/nextcloud/deck/database/DataBaseAdapter.java
+++ app/src/main/java/it/niedermann/nextcloud/deck/database/DataBaseAdapter.java
@@ -426,7 +426,7 @@ public class DataBaseAdapter {
     @WorkerThread
     public UserForAssignment getUserForAssignmentDirectly(long localUserId) {
         SimpleSQLiteQuery query = new SimpleSQLiteQuery(
-                "SELECT acl.type, u.uid as userId " +
+                "SELECT case when acl.type is null then 0 else 1 end as type, u.uid as userId " +
                         "FROM User u " +
                         "left join AccessControl acl on acl.userId = u.localId " +
                         " WHERE u.localId = ? LIMIT 1",
