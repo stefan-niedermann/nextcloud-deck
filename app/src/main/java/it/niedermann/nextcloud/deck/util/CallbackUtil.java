5d8746bc5aa14d324ce5023c202ce2e9d2471997
Stefan Niedermann <info@niedermann.it>
2026-06-20 00:45:25 +0200

    chore: Extend CallbackUtil and prepare for allowing elseRunnables

--- app/src/main/java/it/niedermann/nextcloud/deck/util/CallbackUtil.java
+++ app/src/main/java/it/niedermann/nextcloud/deck/util/CallbackUtil.java
@@ -7,19 +7,35 @@ import androidx.lifecycle.Lifecycle;
 public class CallbackUtil {
 
     public static void runOnUiThread(Fragment fragment, Runnable runnable) {
+        runOnUiThreadOrElse(fragment, runnable, null);
+    }
+
+    public static void runOnUiThreadOrElse(Fragment fragment, Runnable runnable, Runnable elseRunnable) {
         if (fragment.getLifecycle().getCurrentState().isAtLeast(Lifecycle.State.STARTED)) {
             final var activity = fragment.getActivity();
-            runOnUiThread(activity, runnable);
+            runOnUiThreadOrElse(activity, runnable, elseRunnable);
+        } else {
+            if (elseRunnable != null) {
+                elseRunnable.run();
+            }
         }
     }
 
     public static void runOnUiThread(FragmentActivity activity, Runnable runnable) {
+        runOnUiThreadOrElse(activity, runnable, null);
+    }
+
+    public static void runOnUiThreadOrElse(FragmentActivity activity, Runnable runnable, Runnable elseRunnable) {
         if (activity == null) {
             throw new NullPointerException("Activity must not be null.");
         }
 
         if (activity.getLifecycle().getCurrentState().isAtLeast(Lifecycle.State.STARTED)) {
             activity.runOnUiThread(runnable);
+        } else {
+            if (elseRunnable != null) {
+                elseRunnable.run();
+            }
         }
     }
 }
