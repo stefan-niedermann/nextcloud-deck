af6f9ec22c7e34b3d99e6df2e726239476d99430
desperateCoder <a.dawtjan@gmail.com>
2026-08-01 20:52:49 +0200

    refactor(javafx): Fix --purge launcher

--- app/javafx/src/main/java/it/niedermann/nextcloud/deck/javafx/Launcher.java
+++ app/javafx/src/main/java/it/niedermann/nextcloud/deck/javafx/Launcher.java
@@ -8,10 +8,12 @@ public class Launcher {
 
         final var appComponent = DaggerAppComponent.factory().create();
 
+        if (args.length == 1 && "--purge".equals(args[0])) {
+            // TODO Provide Purge-Button in ExceptionDialog
+            appComponent.getPurgeService().purge();
+        }
+
         JavaFxApplication.inject(appComponent.getFxComponentFactory());
         JavaFxApplication.launch(JavaFxApplication.class, args);
-
-        // TODO Provide Purge-Button in ExceptionDialog
-//         appComponent.getPurgeService().purge();
     }
 }
